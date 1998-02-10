e97cbbe9d7bef12023e2e2b6a92380bcadb9a17e
Craig Small <csmall@src.gnome.org>
1998-02-10 02:07:15 +0000

    added repeat values
    svn path=/trunk/; revision=9

--- calendar/gui/cal_struct.h
+++ calendar/gui/cal_struct.h
@@ -1,14 +1,27 @@
+#define MAX_SZ 30
+
+enum RepeatType {
+	Single,
+	Days,
+	Months,
+	WeekDays,
+	MonthDays 
+};
+
+
 struct actionitem {
 	char	date[MAX_SZ];
-	char	time[MAX_SZ];
-}
+	int	time;  /* Minutes past midnight */
+};
 
 struct event {
 	struct actionitem start;
 	struct actionitem end;
 
+	enum RepeatType repeat;
+	int	repeatcount;
 	char	description[MAX_SZ];
 	char	subtype[MAX_SZ];
 	GList	*properties;
-}
+};
 
