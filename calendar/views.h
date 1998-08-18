9f922bcea129fa9bd08639ccf2157b8005754ffc
nobody <nobody@localhost>
1998-08-18 12:15:39 +0000

    This commit was manufactured by cvs2svn to create tag 'LIBGTOP_0_25'.
    svn path=/tags/LIBGTOP_0_25/; revision=324

--- calendar/views.h
+++
@@ -1,4 +0,0 @@
-GtkWidget *day_view_create (GnomeCalendar *gcal);
-GtkWidget *week_view_create (GnomeCalendar *gcal);
-GtkWidget *year_view_create (GnomeCalendar *gcal);
-GtkWidget *tasks_create (GnomeCalendar *gcal);
