70fa0cb32695a4e52637d3d2071ae3f638157c41
Arturo Espinosa <unammx@src.gnome.org>
1998-09-15 04:19:56 +0000

    Got cursors working.
    Got cursors working.
    
    Use the cursors properly on the spreadhseet (gives good feedback
    on what is going to happen).
    
    More work on autofill.
    
    miguel.
    
    svn path=/trunk/; revision=377

--- widgets/misc/e-cursors.h
+++ widgets/misc/e-cursors.h
@@ -1,7 +1,17 @@
 #ifndef CURSORS_H
 #define CURSORS_H
 
-extern GdkCursor  *gnumeric_cursors [];
+typedef struct {
+	GdkCursor *cursor;
+	int       hot_x, hot_y;
+	char      **xpm;
+} GnumericCursorDef;
+
+#define GNUMERIC_CURSOR_FAT_CROSS  0
+#define GNUMERIC_CURSOR_THIN_CROSS 1
+#define GNUMERIC_CURSOR_ARROW      2
+
+extern GnumericCursorDef  gnumeric_cursors [];
 
 void    cursors_init     (void);
 void    cursors_shutdown (void);
