ed611eb8ad12ba752930eed7de96fd955715960f
Martin Baulig <martin@src.gnome.org>
1998-12-02 21:05:38 +0000

    Make it with builddir != srcdir again.
    svn path=/trunk/; revision=499

--- macros/autogen.sh
+++ macros/autogen.sh
@@ -11,7 +11,7 @@ DIE=0
     DIE=1
 }
 
-(grep "^AM_PROG_LIBTOOL" configure.in >/dev/null) && {
+(grep "^AM_PROG_LIBTOOL" $srcdir/configure.in >/dev/null) && {
 (libtool --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "**Error**: You must have "\`libtool\'" installed to compile Gnome."
@@ -21,8 +21,8 @@ DIE=0
 }
 }
 
-grep "^AM_GNU_GETTEXT" configure.in >/dev/null && {
-grep "sed.*POTFILES" configure.in >/dev/null || \
+grep "^AM_GNU_GETTEXT" $srcdir/configure.in >/dev/null && {
+grep "sed.*POTFILES" $srcdir/configure.in >/dev/null || \
 (gettext --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "**Error**: You must have "\`gettext\'" installed to compile Gnome."
