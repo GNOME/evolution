bb503a0bb583f7e6ae1280b64328e2ab97485a87
Bertrand Guiheneuf <bertrand@src.gnome.org>
1999-08-05 21:26:44 +0000

    forgotten file
    svn path=/trunk/; revision=1084

---
+++ tests/test4.c
@@ -0,0 +1,23 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+/* testing mh providers 
+   do not use CamelMhFolder directly.
+   We do it here for test purpose only */
+
+
+
+#include "camel-folder.h"
+#include "camel-mh-folder.h"
+#include "camel.h"
+
+int
+main (int argc, char**argv)
+{
+	CamelFolder *folder;
+
+	/*  folder = gtk_type_new (CAMEL_MH_FOLDER_TYPE); */
+	/* nothing for the moment */
+	
+
+
+}
