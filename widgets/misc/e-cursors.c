ffa1277638311dfbc75f2107a5fcd1f140761f97
Michael Meeks <mmeeks@src.gnome.org>
1999-09-10 20:11:36 +0000

    Cosmetic widget update when loading lots of files, Fixed serious bug in sheet tab editing, New sizing algo.
    svn path=/trunk/; revision=1221

--- widgets/misc/e-cursors.c
+++ widgets/misc/e-cursors.c
@@ -19,17 +19,18 @@ extern GnumericCursorDef  gnumeric_cursors [];
 void    cursors_init      (void);
 void    cursors_shutdown  (void);
 
-#define cursor_set(win,c) \
- do {  									\
-     if (win) 								\
-         gdk_window_set_cursor (win, gnumeric_cursors [c].cursor); 	\
-} while (0)
+#define cursor_set(win,c)					   \
+G_STMT_START {							   \
+     if (win) 							   \
+         gdk_window_set_cursor (win, gnumeric_cursors [c].cursor); \
+} G_STMT_END
 
-#define cursor_set_widget(w,c) \
- do {  									\
-     if (GTK_WIDGET (w)->window) 					\
+#define cursor_set_widget(w,c)							     \
+G_STMT_START {									     \
+     if (GTK_WIDGET (w)->window)						     \
 	gdk_window_set_cursor (GTK_WIDGET (w)->window, gnumeric_cursors [c].cursor); \
-} while (0)
+} G_STMT_END
   
 
 #endif /* GNUMERIC_CURSORS_H */
+
