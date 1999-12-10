863f8aa634005c6400d629c353f5fed485106fce
Arturo Espinosa <unammx@src.gnome.org>
1999-12-10 19:20:39 +0000

    Add missing files
    svn path=/trunk/; revision=1480

---
+++ widgets/e-table-col-dnd.h
@@ -0,0 +1,10 @@
+#ifndef _E_TABLE_COL_DND_H_
+#define _E_TABLE_COL_DND_H_
+
+#define TARGET_ETABLE_COL_TYPE "application/x-etable-column-header"
+
+enum {
+	TARGET_ETABLE_COL_HEADER
+};
+
+#endif /* _E_TABLE_COL_DND_H_ */
