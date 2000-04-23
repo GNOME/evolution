6f31d60289f5d3def0cae26fd7ac0afe4d1f95ab
nobody <nobody@localhost>
2000-04-23 19:19:31 +0000

    This commit was manufactured by cvs2svn to create tag 'GNOME_GAMES_1_1_0'.
    svn path=/tags/GNOME_GAMES_1_1_0/; revision=2566

--- mail/mail-ops.h
+++
@@ -1,5 +0,0 @@
-void fetch_mail (GtkWidget *button, gpointer user_data);
-void send_msg (GtkWidget *button, gpointer user_data);
-void forward_msg (GtkWidget *button, gpointer user_data);
-void reply_to_sender (GtkWidget *button, gpointer user_data);
-void reply_to_all (GtkWidget *button, gpointer user_data);
