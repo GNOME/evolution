28bf3e5f3d74c14bf66aab5cfc9d1c0a26a3daac
NotZed <NotZed@HelixCode.com>
2000-05-16 20:25:40 +0000

    Simple driver program for testing, so i can fix all this really broken
    2000-05-16  NotZed  <NotZed@HelixCode.com>
    
    	* filter-editor.c (main): Simple driver program for testing, so i
    	can fix all this really broken code *sigh*
    
    svn path=/trunk/; revision=3097

--- filter/filter-editor.c
+++ filter/filter-editor.c
@@ -341,14 +341,15 @@ filter_editor_set_rules(FilterEditor *e, GList *rules, GList *systemoptions, GLi
 void
 filter_editor_set_rule_files(FilterEditor *e, const char *systemrules, const char *userrules)
 {
-	GList *rules, *options, *options2;
+	GList *rules, *options = NULL, *options2;
 	xmlDocPtr doc, out, optionset, filteroptions;
 
 	doc = xmlParseFile(systemrules);
 	rules = filter_load_ruleset(doc);
 	options2 = filter_load_optionset(doc, rules);
 	out = xmlParseFile(userrules);
-	options = filter_load_optionset(out, rules);
+	if (out)
+		options = filter_load_optionset(out, rules);
 	
 	filter_editor_set_rules(e, rules, options2, options);
 }
@@ -358,3 +359,25 @@ filter_editor_save_rules(FilterEditor *e, const char *userrules)
 {
 	return filter_write_optionset_file(userrules, e->useroptions);
 }
+
+#ifdef STANDALONE
+
+int main(int argc, char **argv)
+{
+	FilterEditor *fe;
+
+ 	gnome_init("Test", "0.0", argc, argv);
+ 	gdk_rgb_init ();
+ 	gtk_widget_set_default_colormap (gdk_rgb_get_cmap ());
+ 	gtk_widget_set_default_visual (gdk_rgb_get_visual ());
+
+	fe = filter_editor_new();
+	filter_editor_set_rule_files(fe, "/home/notzed/gnome/evolution/filter/filterdescription.xml", "/home/notzed/filters.xml");
+	gtk_widget_show(fe);
+
+	gtk_main();
+
+	return 0;
+}
+
+#endif
