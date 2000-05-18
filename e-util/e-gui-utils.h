4c08e67f41a8069264343daa052002dc4e9de822
Christopher James Lahey <clahey@helixcode.com>
2000-05-18 22:23:46 +0000

    Add imagesdir support.
    2000-05-18  Christopher James Lahey  <clahey@helixcode.com>
    
    	* Makefile.am: Add imagesdir support.
    
    	* e-gui-utils.c, e-gui-utils.h: Added e_create_image_widget for
    	glade use.
    
    svn path=/trunk/; revision=3120

--- e-util/e-gui-utils.h
+++ e-util/e-gui-utils.h
@@ -7,6 +7,7 @@
 void  e_popup_menu                   (GtkMenu *menu, GdkEventButton *event);
 void  e_auto_kill_popup_menu_on_hide (GtkMenu *menu);
 void  e_notice                       (GtkWindow *window, const char *type, const char *format, ...);
+GtkWidget *e_create_image_widget(gchar *name, gchar *string1, gchar *string2, gint int1, gint int2);
 
 
 #endif /* E_GUI_UTILS_H */
--- widgets/misc/e-gui-utils.c
+++ widgets/misc/e-gui-utils.c
@@ -11,7 +11,10 @@
 #include <gtk/gtksignal.h>
 #include <libgnomeui/gnome-messagebox.h>
 #include <libgnomeui/gnome-stock.h>
+#include <gnome.h>
 #include "e-gui-utils.h"
+#include <gdk-pixbuf/gdk-pixbuf.h>
+#include <gdk-pixbuf/gnome-canvas-pixbuf.h>
 
 void
 e_notice (GtkWindow *window, const char *type, const char *format, ...)
@@ -61,4 +64,49 @@ e_popup_menu (GtkMenu *menu, GdkEventButton *event)
 	gtk_menu_popup (menu, NULL, NULL, 0, NULL, event->button, event->time);
 }
 
+GtkWidget *e_create_image_widget(gchar *name,
+				 gchar *string1, gchar *string2,
+				 gint int1, gint int2)
+{
+	char *filename;
+	GdkPixbuf *pixbuf;
+	double width, height;
+	GtkWidget *canvas, *alignment;
+	if (string1) {
+		if (*string1 == '/')
+			filename = g_strdup(string1);
+		else
+			filename = g_concat_dir_and_file(EVOLUTION_IMAGES, string1);
+		pixbuf = gdk_pixbuf_new_from_file(filename);
+		width = gdk_pixbuf_get_width(pixbuf);
+		height = gdk_pixbuf_get_height(pixbuf);
+
+		canvas = gnome_canvas_new_aa();
+		GTK_OBJECT_UNSET_FLAGS(GTK_WIDGET(canvas), GTK_CAN_FOCUS);
+		gnome_canvas_item_new(gnome_canvas_root(GNOME_CANVAS(canvas)),
+				      gnome_canvas_pixbuf_get_type(),
+
+				      "pixbuf", pixbuf,
+				      NULL);
+
+		alignment = gtk_widget_new(gtk_alignment_get_type(),
+					   "child", canvas,
+					   "xalign", (double) 0,
+					   "yalign", (double) 0,
+					   "xscale", (double) 0,
+					   "yscale", (double) 0,
+					   NULL);
+	
+		gtk_widget_set_usize(canvas, width, height);
+
+		gdk_pixbuf_unref(pixbuf);
+
+		gtk_widget_show(canvas);
+		gtk_widget_show(alignment);
+		g_free(filename);
+
+		return alignment;
+	} else
+		return NULL;
+}
 
