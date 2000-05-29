6c365b1f6d80de93920dd559322beca472bd601c
Martin Baulig <baulig@suse.de>
2000-05-29 15:17:17 +0000

    If `USE_GNOME_2_MACROS' is set, use the GNOME 2.0 macros from the
    2000-05-29  Martin Baulig  <baulig@suse.de>
    
    	* gnome-autogen.sh: If `USE_GNOME_2_MACROS' is set, use the
    	GNOME 2.0 macros from the `macros2' directory.
    
    svn path=/trunk/; revision=3259

--- macros/gnome-autogen.sh
+++ macros/gnome-autogen.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-export GNOME_COMMON_MACROS_DIR=`gnome-config --datadir`/aclocal/gnome-macros
+if test -n "$USE_GNOME_2_MACROS" ; then
+  export GNOME_COMMON_MACROS_DIR=`gnome-config --datadir`/aclocal/gnome2-macros
+else
+  export GNOME_COMMON_MACROS_DIR=`gnome-config --datadir`/aclocal/gnome-macros
+fi
+
 export ACLOCAL_FLAGS="-I $GNOME_COMMON_MACROS_DIR $ACLOCAL_FLAGS"
 . $GNOME_COMMON_MACROS_DIR/autogen.sh
 
