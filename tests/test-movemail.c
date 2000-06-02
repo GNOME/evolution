87593b97975f7fb05e799c762d99c06e31bfb2d8
Christopher James Lahey <clahey@helixcode.com>
2000-06-02 19:27:02 +0000

    Reverted removal of e_setup_base_dir.
    2000-06-02  Christopher James Lahey  <clahey@helixcode.com>
    
    	* tests/test-movemail.c: Reverted removal of e_setup_base_dir.
    
    svn path=/trunk/; revision=3399

--- tests/test-movemail.c
+++ tests/test-movemail.c
@@ -48,6 +48,7 @@ int main (int argc, char **argv)
 		fprintf (stderr, "Usage: test-movemail [--delete] url\n");
 		exit (1);
 	}
+	e_setup_base_dir ();
 	session = camel_session_new (auth_callback);
 
 	ex = camel_exception_new ();
