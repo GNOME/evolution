da56d618862bd8894a15aba42111864da3c3abd3
nobody <nobody@localhost>
2000-08-02 22:26:45 +0000

    This commit was manufactured by cvs2svn to create tag 'gnorpm-glibwww-branch-anchor'.
    svn path=/tags/gnorpm-glibwww-branch-anchor/; revision=4488

--- addressbook/gui/component/e-ldap-server-dialog.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __E_LDAP_SERVER_DIALOG_H__
-#define __E_LDAP_SERVER_DIALOG_H__
-
-#include "e-ldap-storage.h"
-
-void e_ldap_server_editor_show(ELDAPServer *server);
-
-#endif /* __E_LDAP_SERVER_DIALOG_H__ */
