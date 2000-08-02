a6a73e2b7d45adeaa57ab76a30150f5534178e27
nobody <nobody@localhost>
2000-08-02 22:26:45 +0000

    This commit was manufactured by cvs2svn to create tag 'GTKHTML_0_6_1'.
    svn path=/tags/GTKHTML_0_6_1/; revision=4485

--- calendar/cal-util/icalendar.h
+++
@@ -1,13 +0,0 @@
-#ifndef ICALENDAR_H
-#define ICALENDAR_H
-
-#include <ical.h>
-#include <cal-util/calobj.h>
-
-
-
-iCalObject *ical_object_create_from_icalcomponent (icalcomponent* comp);
-
-
-
-#endif
