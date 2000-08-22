6bf0dab0770162e5eab74dd07f6f15089ea6f92e
nobody <nobody@localhost>
2000-08-22 12:05:30 +0000

    This commit was manufactured by cvs2svn to create tag 'CONTROL_CENTER_1_3_1'.
    svn path=/tags/CONTROL_CENTER_1_3_1/; revision=4928

--- calendar/cal-util/icalendar-save.h
+++
@@ -1,13 +0,0 @@
-#ifndef ICALENDAR_SAVE_H
-#define ICALENDAR_SAVE_H
-
-#include <ical.h>
-#include <cal-util/calobj.h>
-
-
-
-icalcomponent *icalcomponent_create_from_ical_object (iCalObject *ical);
-
-
-
-#endif
