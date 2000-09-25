d4c1adb83923228f54dbd8eac67215c799ca8f5c
Nat Friedman <nat@helixcode.com>
2000-09-25 18:34:05 +0000

    Added a ton of new prefixes and suffixes.
    2000-09-25  Nat Friedman  <nat@helixcode.com>
    
    	* ename/e-name-western-tables.h: Added a ton of new prefixes and
    	suffixes.
    
    svn path=/trunk/; revision=5571

--- e-util/ename/e-name-western-tables.h
+++ e-util/ename/e-name-western-tables.h
@@ -9,24 +9,42 @@ char *e_name_western_pfx_table[] = {
 	"mister", "miss.", "mr.", "mrs.", "ms.",
 	"miss", "mr", "mrs", "ms", "sir", 
 	"professor", "prof.", "dr", "dr.", "doctor",
-	"reverend", "president", "judge", "senator",
+	"judge", "justice", "chief justice", 
 	"congressman", "congresswoman", "commander",
-	"lieutenant", "colonel", "major", "general",  
+	"lieutenant", "lt.", "colonel", "col.", "major", "maj.",
+	"general", "gen.", "admiral", "admr.", "sergeant", "sgt.",
+	"lord", "lady", "baron", "baroness", "duke", "duchess",
+	"king", "queen", "prince", "princess",
 
-	"the honorable", "the reverend", "his holiness",
-	"his eminence",
-	
+	"the most honorable", "the honorable",
+	"the reverend", "his holiness",
+	"his eminence", "his majesty", "her majesty", 
+	"his grace", "her grace",
+
+	"president", "vice president", "secretary", "undersecretary",
+	"consul", "ambassador",
+
+	"senator", "saint", "st.", "pastor", "deacon",
+	"father", "bishop", "archbishop", "cardinal", "pope",
+	"reverend", "rev.", "rabbi", 
 
 	/*
 	 * French.
 	 */
-	"monsieur", "mr.", "mademoiselle", "melle.",
-	"madame", "mme.", "professeur",
+	"monsieur", "m.", "mademoiselle", "melle",
+	"madame", "mme", "professeur", "dauphin", "dauphine",
+
+	/*
+	 * German
+	 */
+	"herr", "frau", "fraulein", "herr doktor", "doktor frau", "doktor frau doktor",
+	"frau doktor",
+ 
 
 	/*
 	 * Spanish.
 	 */
-	"senor", "senora", "senorita",
+	"senor", "senora", "sra.", "senorita", "srita.", 
 
 	NULL};
 
@@ -35,8 +53,10 @@ char *e_name_western_sfx_table[]  = {
 	/*
 	 * English.
 	 */
-	"junior", "senior", "jr", "sr", "I", "II", "III", "IV", "V",
-	"phd", "ms", "md", "esq", "esq.", "esquire",
+         "junior", "senior", "jr", "sr", "I", "II", "III", "IV", "V",
+	 "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV",
+	 "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII",
+	 "phd", "ms", "md", "esq", "esq.", "esquire",
 
 	NULL};
 
