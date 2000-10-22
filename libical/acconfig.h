bd33f182ccfbd4a69617610d922294b6871f5068
nobody <nobody@localhost>
2000-10-22 06:03:03 +0000

    This commit was manufactured by cvs2svn to create tag 'GAL_0_2_1'.
    svn path=/tags/GAL_0_2_1/; revision=6103

--- libical/acconfig.h
+++
@@ -1,7 +0,0 @@
-/* Define to make icalerror_* calls abort instead of internally
-   signalling an error */
-#undef ICAL_ERRORS_ARE_FATAL
-
-/* Define to make icalcluster_commit() save to a temp file and mv to
-   the original file instead of writing to the orig file directly */
-#undef ICAL_SAFESAVES
