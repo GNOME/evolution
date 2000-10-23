75eec4a1bb02390332f812ceb91ff97ce60e133a
Kjartan Maraas <kmaraas@gnome.org>
2000-10-23 23:02:35 +0000

    moved #include <config.h> to the top.
    2000-10-24  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* addressbook/conduit/address-conduit-control-applet.c:
    	moved #include <config.h> to the top.
    
    svn path=/branches/evolution-0-6-branch/; revision=6135

--- addressbook/conduit/address-conduit-control-applet.c
+++ addressbook/conduit/address-conduit-control-applet.c
@@ -22,12 +22,12 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#include <config.h>
 #include <pwd.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <gnome.h>
 
-#include <config.h>
 #include <capplet-widget.h>
 
 #include <gpilotd/gnome-pilot-client.h>
