4613b4e6951f06eb5634c3993e1639ecca201948
nobody <nobody@localhost>
2000-10-30 05:21:59 +0000

    This commit was manufactured by cvs2svn to create tag 'GNOME-VFS_0_4'.
    svn path=/tags/GNOME-VFS_0_4/; revision=6262

--- addressbook/gui/component/addressbook.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ADDRESSBOOK_H__
-#define __ADDRESSBOOK_H__
-
-#include <bonobo/bonobo-control.h>
-
-BonoboControl *addressbook_factory_new_control  (void);
-void           addressbook_factory_init         (void);
-
-#endif /* __ADDRESSBOOK_H__ */
