edacbf9374dfaf1836c7dd3fab063eb8499bac3c
nobody <nobody@localhost>
2000-10-30 05:21:59 +0000

    This commit was manufactured by cvs2svn to create tag 'GNOME-VFS_0_4_1'.
    svn path=/tags/GNOME-VFS_0_4_1/; revision=6263

--- widgets/table/e-cell-string.c
+++
@@ -1,9 +0,0 @@
-ECell *
-e_cell_string_new (void)
-{
-	ECell *ecell;
-
-	ecell = gtk_type_new (ecell);
-
-	return ecell;
-}
--- widgets/table/e-table-column-model.h
+++
@@ -1,5 +0,0 @@
-
-class ETableColumnModel {
-	virtual void add_column (ETableCol *et) = 0;
-	virtual ETableCol *get_column (int column);
-	virtual 
