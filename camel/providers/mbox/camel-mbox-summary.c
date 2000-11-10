977338752bf5bba40472862677fff1d7eca2a1ea
Dan Winship <danw@src.gnome.org>
2000-11-10 21:32:24 +0000

    put a space before a single-digit day of the month since it seems some
    	* providers/mbox/camel-mbox-summary.c
    	(camel_mbox_summary_build_from): put a space before a single-digit
    	day of the month since it seems some mailers are fantastically
    	picky about this. (bugs.gnome.org #27232)
    
    svn path=/trunk/; revision=6538

--- camel/providers/mbox/camel-mbox-summary.c
+++ camel/providers/mbox/camel-mbox-summary.c
@@ -609,7 +609,7 @@ camel_mbox_summary_build_from(struct _header_raw *header)
 	/*memcpy(&tm, gmtime(&thetime), sizeof(tm));*/
 	gmtime_r(&thetime, &tm);
 
-	g_string_sprintfa(out, " %s %s %d %02d:%02d:%02d %4d\n",
+	g_string_sprintfa(out, " %s %s %2d %02d:%02d:%02d %4d\n",
 			  tz_days[tm.tm_wday],
 			  tz_months[tm.tm_mon], tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, tm.tm_year + 1900);
 
