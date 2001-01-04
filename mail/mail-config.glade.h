b74ccc5a87b3aded415dc721f2e6312e07066f55
Jeffrey Stedfast <fejj@src.gnome.org>
2001-01-04 20:44:00 +0000

    rebuilt this, glade crashed...so uh, maybe this is ok now? I dunno
    svn path=/trunk/; revision=7255

--- mail/mail-config.glade.h
+++ mail/mail-config.glade.h
@@ -4,56 +4,25 @@
  * DO NOT compile it as part of your application.
  */
 
-gchar *s = N_("Mail Configuration Druid");
-gchar *s = N_("Mail Configuration");
-gchar *s = N_("Welcome to the Evolution Mail Configuration Druid.\n"
-              "\n"
-              "Click \"Next\" to begin. ");
-gchar *s = N_("Identity");
-gchar *s = N_("Required");
-gchar *s = N_("Full Name:");
-gchar *s = N_("Email Address:");
-gchar *s = N_("Optional");
-gchar *s = N_("Organization:");
-gchar *s = N_("Signature file:");
-gchar *s = N_("Reply-to:");
-gchar *s = N_("Receiving Email");
-gchar *s = N_("Server Type: ");
-gchar *s = N_("Server Configuration");
-gchar *s = N_("Host:");
-gchar *s = N_("Username:");
-gchar *s = N_("Path:");
-gchar *s = N_("Delete mail from server");
-gchar *s = N_("Authentication");
-gchar *s = N_("Authentication");
-gchar *s = N_("Authentication Type:");
-gchar *s = N_("Password:");
-gchar *s = N_("Remember my password");
-gchar *s = N_("Sending Email");
-gchar *s = N_("Server Type: ");
-gchar *s = N_("Server Configuration");
-gchar *s = N_("Host:");
-gchar *s = N_("Server requires authentication");
-gchar *s = N_("Account Management");
-gchar *s = N_("Account Information");
-gchar *s = N_("Name:");
-gchar *s = N_("Make this my default account");
-gchar *s = N_("Done");
-gchar *s = N_("Congratulations, your mail configuration is complete.\n"
-              "\n"
-              "You are now ready to send and receive email \n"
-              "using Evolution. \n"
-              "\n"
-              "Click \"Finish\" to save your settings.");
-gchar *s = N_("Evolution Mail Configuration");
-gchar *s = N_("Default");
-gchar *s = N_("Accounts");
+gchar *s = N_("Identities");
+gchar *s = N_("Address");
+gchar *s = N_("Organization");
+gchar *s = N_("Signature File");
 gchar *s = N_("Add");
 gchar *s = N_("Edit");
 gchar *s = N_("Delete");
-gchar *s = N_("Mail");
+gchar *s = N_("Identities");
 gchar *s = N_("Sources");
 gchar *s = N_("Add");
 gchar *s = N_("Edit");
 gchar *s = N_("Delete");
-gchar *s = N_("News");
+gchar *s = N_("Mail Sources");
+gchar *s = N_("Mail Transport");
+gchar *s = N_("News Servers");
+gchar *s = N_("Add");
+gchar *s = N_("Edit");
+gchar *s = N_("Delete");
+gchar *s = N_("News Sources");
+gchar *s = N_("Send messages in HTML format");
+gchar *s = N_("Mark message as seen [ms]: ");
+gchar *s = N_("Other");
