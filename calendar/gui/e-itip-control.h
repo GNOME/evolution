bf03075d1722e8d97ed8ad9c9448530036aab523
nobody <nobody@localhost>
2001-01-11 16:04:13 +0000

    This commit was manufactured by cvs2svn to create tag 'GNOME-VFS_PRE_0_5'.
    svn path=/tags/GNOME-VFS_PRE_0_5/; revision=7391

--- calendar/gui/e-itip-control.h
+++
@@ -1,8 +0,0 @@
-#ifndef __E_ITIP_CONTROL_H__
-#define __E_ITIP_CONTROL_H__
-
-#include <bonobo/bonobo-control.h>
-
-void             e_itip_control_factory_init (void);
-
-#endif /* __E_ITIP_CONTROL_H__ */
