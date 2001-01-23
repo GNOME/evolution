e31cd9c18fe326dae5fe4c0b26a47cc75448a61c
Jeffrey Stedfast <fejj@ximian.com>
2001-01-23 21:23:35 +0000

    Make sure the source->url is not NULL (which is perfectly valid).
    2001-01-23  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* mail-send-recv.c (build_dialogue): Make sure the source->url is
    	not NULL (which is perfectly valid).
    
    svn path=/trunk/; revision=7756

--- mail/mail-send-recv.c
+++ mail/mail-send-recv.c
@@ -1,3 +1,24 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ *  Authors: Michael Zucchi <NotZed@ximian.com>
+ *
+ *  Copyright 2001 Ximian, Inc. (www.ximian.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *
+ */
 
 #include <stdio.h>
 #include <string.h>
@@ -170,7 +191,12 @@ static struct _send_data *build_dialogue(GSList *sources, CamelFolder *outbox, c
 	row = 0;
 	while (sources) {
 		MailConfigService *source = sources->data;
-
+		
+		if (!source->url) {
+			sources = sources->next;
+			continue;
+		}
+		
 		info = g_malloc0(sizeof(*info));
 		/* imap is handled differently */
 		if (!strncmp(source->url, "imap:", 5))
