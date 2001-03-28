e3ac0bad97d70498defff189df7b2d6f0f5918d2
Dan Winship <danw@src.gnome.org>
2001-03-28 22:15:35 +0000

    add a bit mentioning that if configure claims you don't have something
    	* README: add a bit mentioning that if configure claims you don't
    	have something installed when you think you do, that it's probably
    	because you installed it in the wrong prefix, or because you need
    	a -devel package.
    
    	* configure.in: Add a new macro EVO_CHECK_LIB that checks for a
    	gnome-config-based library of a given version or later, correctly,
    	and if doesn't find it, suggests that you consult the README. Fix
    	the various library checks to use this. Meanwhile, remove a bunch
    	of old cruft and reorganize a little.
    
    	* acconfig.h: Remove cruft
    
    svn path=/trunk/; revision=8997

--- acconfig.h
+++ acconfig.h
@@ -12,24 +12,18 @@
 #undef HAVE_LDAP
 #undef HAVE_TIMEZONE
 #undef HAVE_TM_GMTOFF
-#undef USING_OAF
 #undef HAVE_KRB4
+#undef NEED_KRB_SENDAUTH_PROTO
 #undef HAVE_KRB5
 #undef HAVE_NSS
 #undef MOVEMAIL_PATH
-#undef USING_GNOME_PRINT_0_20
 #undef USE_DOT
 #undef USE_FCNTL
 #undef USE_FLOCK
-#undef NEED_KRB_SENDAUTH_PROTO
-#undef HAVE_ISBLANK
 #undef ENABLE_NNTP
 
 /* Sub-version identification string.  */
 #undef SUB_VERSION
-
-/* Define this if you want to build against the development gtk */
-#undef HAVE_DEVGTK
 
 /* Define if ctime_r takes three arguments */
 #undef CTIME_R_THREE_ARGS
