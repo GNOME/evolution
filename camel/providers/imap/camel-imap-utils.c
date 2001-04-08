9582b8384d61ecb22d8a4f9b95685b54312ba68b
Peter Williams <peterw@ximian.com>
2001-04-08 00:38:00 +0000

    It would be a good idea to recognize '\0'. Before, this code was running
    001-04-07  Peter Williams  <peterw@ximian.com>
    
    	* providers/imap/camel-imap-utils.c (imap_parse_string_generic): It
    	would be a good idea to recognize '\0'. Before, this code was running
    	off the ends of strings and intermittently coredumping (if it didn't
    	hit an imap_atom_char first) -- whoops!
    
    svn path=/trunk/; revision=9202

--- camel/providers/imap/camel-imap-utils.c
+++ camel/providers/imap/camel-imap-utils.c
@@ -275,7 +275,7 @@ imap_parse_string_generic (char **str_p, int *len, int type)
 		return NULL;
 	} else if (type == IMAP_ASTRING &&
 		   imap_is_atom_char ((unsigned char)*str)) {
-		while (imap_is_atom_char ((unsigned char)*str))
+		while (imap_is_atom_char ((unsigned char)*str) && *str != '\0')
 			str++;
 
 		*len = str - *str_p;
