d0243a91821d311699347c11d454314bdbdfc0fe
Dan Winship <danw@src.gnome.org>
2001-05-16 22:31:49 +0000

    add E_TEXT_TO_HTML_ESCAPE_8BIT to the flags
    	* mail-format.c (mail_format_raw_message): add
    	E_TEXT_TO_HTML_ESCAPE_8BIT to the flags
    
    svn path=/trunk/; revision=9860

--- mail/mail-format.c
+++ mail/mail-format.c
@@ -189,7 +189,7 @@ mail_format_raw_message (CamelMimeMessage *mime_message, MailDisplay *md)
 	g_return_if_fail (CAMEL_IS_MIME_MESSAGE (mime_message));
 	
 	text = get_data_wrapper_text (CAMEL_DATA_WRAPPER (mime_message));
-	html = e_text_to_html (text, E_TEXT_TO_HTML_CONVERT_NL | E_TEXT_TO_HTML_CONVERT_SPACES);
+	html = e_text_to_html (text, E_TEXT_TO_HTML_CONVERT_NL | E_TEXT_TO_HTML_CONVERT_SPACES | E_TEXT_TO_HTML_ESCAPE_8BIT);
 	gtk_html_write (md->html, md->stream, "<tt>", 4);
 	gtk_html_write (md->html, md->stream, html, strlen (html));
 	gtk_html_write (md->html, md->stream, "</tt>", 5);
