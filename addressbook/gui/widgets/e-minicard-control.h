594c74e8dce1e343843ec77ebd4f3e583b0d1c89
nobody <nobody@localhost>
2001-05-25 00:59:35 +0000

    This commit was manufactured by cvs2svn to create tag 'XST_0_5_0'.
    svn path=/tags/XST_0_5_0/; revision=9977

--- addressbook/gui/widgets/e-minicard-control.h
+++
@@ -1,8 +0,0 @@
-#ifndef __E_MINICARD_CONTROL_H__
-#define __E_MINICARD_CONTROL_H__
-
-#include <bonobo/bonobo-control.h>
-
-void             e_minicard_control_factory_init (void);
-
-#endif /* __E_MINICARD_CONTROL_H__ */
