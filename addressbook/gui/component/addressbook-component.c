681b9172bc8a48394377562f58701741ebcc85be
Jeffrey Stedfast <fejj@ximian.com>
2001-06-14 20:17:43 +0000

    Set the "expoted_dnd_types" to NULL in the folder_types.
    2001-06-14  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* gui/component/addressbook-component.c: Set the
    	"expoted_dnd_types" to NULL in the folder_types.
    
    svn path=/trunk/; revision=10237

--- addressbook/gui/component/addressbook-component.c
+++ addressbook/gui/component/addressbook-component.c
@@ -63,7 +63,7 @@ static char *accepted_dnd_types[] = {
 };
 
 static const EvolutionShellComponentFolderType folder_types[] = {
-	{ "contacts", "evolution-contacts.png", accepted_dnd_types },
+	{ "contacts", "evolution-contacts.png", accepted_dnd_types, NULL },
 	{ NULL, NULL, NULL, NULL }
 };
 
