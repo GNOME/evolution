131c4514f73e4423bc3be32c3dd0338853f6ac05
Radek Doulik <rodo@ximian.com>
2001-06-19 14:07:04 +0000

    add column with &nbsp; between header name and value
    2001-06-19  Radek Doulik  <rodo@ximian.com>
    
    	* mail-format.c (write_field_row_begin): add column with &nbsp;
    	between header name and value
    
    svn path=/trunk/; revision=10286

--- mail/mail-format.c
+++ mail/mail-format.c
@@ -630,7 +630,7 @@ write_field_row_begin (const char *name, gint flags, GtkHTML *html, GtkHTMLStrea
 				 bold ? "<b>" : "", encoded_name,
 				 bold ? "</b>" : "");
 	} else {
-		mail_html_write (html, stream, "<tr><%s align=\"right\" valign=\"top\">%s:</%s><td>",
+		mail_html_write (html, stream, "<tr><%s align=\"right\" valign=\"top\">%s:</%s><td>&nbsp;</td><td>",
 				 bold ? "th" : "td", encoded_name, bold ? "th" : "td");
 	}
 
