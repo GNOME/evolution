c6178d76a5af97cb03460cca6c13b5bb9bef44a3
Anna Marie Dirks <anna@ximian.com>
2001-08-09 23:20:36 +0000

    added more padding between the vertical spacer and the icons for the
    2001-08-09  Anna Marie Dirks <anna@ximian.com>
    	* my-evolution-html.h : added more padding between the vertical
    	spacer and the icons for the mail/appointments/tasks summaries.
    
    svn path=/trunk/; revision=11847

--- my-evolution/my-evolution-html.h
+++ my-evolution/my-evolution-html.h
@@ -18,22 +18,23 @@
 #define HTML_1 "<html><head></head><body background=\"bcg.png\" "\
 "bgcolor=\"white\" link=\"#314e6c\" alink=\"black\" vlink=\"#314e6c\">"\
 "<img src=\"empty.gif\" alt=\"\" width=\"10\" height=\"20\">"\
-"<table border=\"0\" numcols=\"4\" cellspacing=\"0\" cellpadding=\"0\" width=\"100%\">"\
+"<table border=\"0\" numcols=\"5\" cellspacing=\"0\" cellpadding=\"0\" width=\"100%\">"\
 "<tbody><tr>"
 #endif
 
 /* Needs a stringified date */
-#define HTML_2 "<td align=\"Right\" colspan=\"4\"><b>%s</b><br> <img src=\"empty.gif\" width=\"1\" height=\"3\"></td>"
+#define HTML_2 "<td align=\"Right\" colspan=\"5\"><b>%s</b><br> <img src=\"empty.gif\" width=\"1\" height=\"3\"></td>"
 
-#define HTML_3 "</tr><tr><td colspan=\"4\" bgcolor=\"#000000\"><img src=\"empty.gif\" width=\"1\" height=\"1\"></td></tr>"\
+#define HTML_3 "</tr><tr><td colspan=\"5\" bgcolor=\"#000000\"><img src=\"empty.gif\" width=\"1\" height=\"1\"></td></tr>"\
 "<tr valign=\"Top\">" \
-"<td width=\"100%\">"\
+"<td  width=\"100%\">"\
 
 /* Weather stuff goes here */
 
 /* RDF Stuff goes here */
 
 #define HTML_4 "</td><td>&#160;</td><td width=\"1\" bgcolor=\"#000000\"><img src=\"empty.gif\" width=\"1\" height=\"1\"></td>"\
+"<td width=\"15\" background=\"pattern.png\">&nbsp&nbsp&nbsp</td>"\
 "<td width=\"0\" background=\"pattern.png\">"
 
 /* Mail stuff ici s'il vous plait */
@@ -50,3 +51,4 @@
 "<tr bgcolor=\"#000000\"><td colspan=\"5\"><img src=\"empty.gif\" alt=\"\" width=\"10\" height=\"1\"></td></tr>"\
 "</tbody></table></body></html>"
 #endif
+
