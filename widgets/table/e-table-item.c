c4b24c895e0a351315b8dea294983bbaf2b8dfc1
Christopher James Lahey <clahey@ximian.com>
2001-08-27 15:32:14 +0000

    Fixed this memmove to subtract the count as well as the row for the number
    2001-08-27  Christopher James Lahey  <clahey@ximian.com>
    
    	* e-table-item.c (eti_table_model_rows_deleted): Fixed this
    	memmove to subtract the count as well as the row for the number of
    	rows to move.
    
    svn path=/trunk/; revision=12482

--- widgets/table/e-table-item.c
+++ widgets/table/e-table-item.c
@@ -820,7 +820,7 @@ eti_table_model_rows_deleted (ETableModel *table_model, int row, int count, ETab
 	eti->rows = e_table_model_row_count (eti->table_model);
 
 	if (eti->height_cache)
-		memmove(eti->height_cache + row, eti->height_cache + row + count, (eti->rows - row) * sizeof(int));
+		memmove(eti->height_cache + row, eti->height_cache + row + count, (eti->rows - row - count) * sizeof(int));
 
 	eti->needs_compute_height = 1;
 	e_canvas_item_request_reflow (GNOME_CANVAS_ITEM (eti));
