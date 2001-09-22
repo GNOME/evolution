3b42fe201d6920329a135fc276385a60c2a0d8c7
Chris Toshok <toshok@ximian.com>
2001-09-22 21:50:02 +0000

    when creating the cards, don't display any of the subname fields (surname,
    2001-09-22  Chris Toshok  <toshok@ximian.com>
    
    	* gui/widgets/e-minicard.c (remodel): when creating the cards,
    	don't display any of the subname fields (surname, given, middle,
    	suffix, etc.)
    
    svn path=/trunk/; revision=13083

--- addressbook/gui/widgets/e-minicard.c
+++ addressbook/gui/widgets/e-minicard.c
@@ -780,7 +780,7 @@ remodel( EMinicard *e_minicard )
 		list = e_minicard->fields;
 		e_minicard->fields = NULL;
 
-		for(field = E_CARD_SIMPLE_FIELD_FULL_NAME; field != E_CARD_SIMPLE_FIELD_LAST - 2 && count < 5; field++) {
+		for(field = E_CARD_SIMPLE_FIELD_FULL_NAME; field != E_CARD_SIMPLE_FIELD_LAST - 6 && count < 5; field++) {
 			EMinicardField *minicard_field = NULL;
 
 			if (field == E_CARD_SIMPLE_FIELD_FAMILY_NAME)
