56b24983e2413d5863442af43f10150808a8a56f
Iain Holmes <iain@src.gnome.org>
2001-09-26 19:37:32 +0000

    Add copyright notices
    svn path=/trunk/; revision=13157

--- my-evolution/e-summary-offline-handler.h
+++ my-evolution/e-summary-offline-handler.h
@@ -1,9 +1,27 @@
-/*
- * e-summary-offline-handler.h:
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
+/* e-summary-offline-handler.h
  *
- * Copyright (C) 2001  Ximian, Inc.
+ * Copyright (C) 2001 Ximian, Inc.
  *
- * Authors: Iain Holmes  <iain@ximian.com>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Authors: 
+ * Ettore Perazzoli <ettore@ximian.com>
+ * Dan Winship <danw@ximian.com>
+ * Iain Holmes  <iain@ximian.com>
  */
 
 #ifndef __E_SUMMARY_OFFLINE_HANDLER_H__
