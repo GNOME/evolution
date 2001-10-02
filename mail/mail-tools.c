ecff8f1e181f5b92df7435bb9141758366168ab0
nobody <nobody@localhost>
2001-10-02 21:57:22 +0000

    This commit was manufactured by cvs2svn to create tag 'EVOLUTION_0_15'.
    svn path=/tags/EVOLUTION_0_15/; revision=13351

--- mail/mail-tools.c
+++ mail/mail-tools.c
@@ -315,7 +315,7 @@ mail_tool_uri_to_folder (const char *uri, CamelException *ex)
 		if (offset)
 			folder = camel_store_get_trash (store, ex);
 		else
-			folder = camel_store_get_folder (store, name, 0, ex);
+			folder = camel_store_get_folder (store, name, CAMEL_STORE_FOLDER_CREATE, ex);
 		camel_object_unref (CAMEL_OBJECT (store));
 	}
 	
