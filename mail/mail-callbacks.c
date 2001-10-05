301815b5e307a83e49218e2e2aa5ce9971717bc0
Larry Ewing <lewing@ximian.com>
2001-10-05 16:55:15 +0000

    use e_msg_composer_new_with_message rather than e_msg_composer_new. This
    2001-10-05  Larry Ewing  <lewing@ximian.com>
    
    	* mail-callbacks.c (mail_generate_reply): use
    	e_msg_composer_new_with_message rather than e_msg_composer_new.
    	This way we bring in all the attachment of the message we are
    	replying to.
    
    svn path=/trunk/; revision=13444

--- mail/mail-callbacks.c
+++ mail/mail-callbacks.c
@@ -772,7 +772,7 @@ mail_generate_reply (CamelFolder *folder, CamelMimeMessage *message, const char 
 	time_t date;
 	const int max_subject_length = 1024;
 	
-	composer = e_msg_composer_new ();
+	composer = e_msg_composer_new_with_message (message);
 	if (!composer)
 		return NULL;
 	
