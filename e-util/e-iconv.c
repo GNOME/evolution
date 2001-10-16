6e9c98ceef8731142d568d851cd0736296bdde5e
6 <NotZed@Ximian.com>
2001-10-16 19:44:09 +0000

    Want to check nn not in for end of list condition.
    2001-10-16    <NotZed@Ximian.com>
    
            * gal/util/e-iconv.c (flush_entry): Want to check nn not in for
            end of list condition.
    
    svn path=/trunk/; revision=13699

--- e-util/e-iconv.c
+++ e-util/e-iconv.c
@@ -317,7 +317,7 @@ flush_entry(struct _iconv_cache *ic)
 
 	in = (struct _iconv_cache_node *)ic->open.head;
 	nn = in->next;
-	while (in) {
+	while (nn) {
 		if (in->ip != (iconv_t)-1) {
 			g_hash_table_remove(iconv_cache_open, in->ip);
 			iconv_close(in->ip);
