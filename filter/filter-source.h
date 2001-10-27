f84e94ae7ff672fc1e0b7a3a3081f7b424be6e35
Ettore Perazzoli <ettore@src.gnome.org>
2001-10-27 17:59:02 +0000

    Fixing the license text.
    svn path=/trunk/; revision=14215

--- filter/filter-source.h
+++ filter/filter-source.h
@@ -1,27 +1,24 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-
 /*
  * filter-source.h
  *
  * Copyright (C) 2001 Ximian, Inc.
  *
- * Developed by Jon Trowbridge <trow@ximian.com>
- */
-
-/*
+ * Authors: Jon Trowbridge <trow@ximian.com>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General Public
  * License as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #ifndef __FILTER_SOURCE_H__
