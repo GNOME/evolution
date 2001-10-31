0051272a4e89d010ed2bfa118c6e692a79fd8647
Damon Chaplin <damon@ximian.com>
2001-10-31 05:37:20 +0000

    disconnect signals added in append_page(). Fixes Gtk-Critical warning
    2001-10-30  Damon Chaplin  <damon@ximian.com>
    
    	* gui/dialogs/comp-editor.c (comp_editor_remove_page): disconnect
    	signals added in append_page(). Fixes Gtk-Critical warning about
    	GtkAccelGroup being added twice to a window.
    
    svn path=/trunk/; revision=14518

--- calendar/gui/dialogs/comp-editor.c
+++ calendar/gui/dialogs/comp-editor.c
@@ -583,6 +583,11 @@ comp_editor_remove_page (CompEditor *editor, CompEditorPage *page)
 	priv = editor->priv;
 
 	page_widget = comp_editor_page_get_widget (page);
+
+	/* Disconnect all the signals added in append_page(). */
+	gtk_signal_disconnect_by_data (GTK_OBJECT (page), editor);
+	gtk_signal_disconnect_by_data (GTK_OBJECT (page_widget), page);
+
 	page_num = gtk_notebook_page_num (priv->notebook, page_widget);
 	gtk_notebook_remove_page (priv->notebook, page_num);
 
