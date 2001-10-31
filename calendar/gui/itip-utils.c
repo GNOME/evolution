f758c4d59793b5b00936c56068dc5fb2dbae0b47
JP Rosevear <jpr@ximian.com>
2001-10-31 08:38:01 +0000

    send as mixed rather than alternative
    2001-10-31  JP Rosevear  <jpr@ximian.com>
    
    	* gui/itip-utils.c (itip_send_comp): send as mixed rather than
    	alternative
    
    svn path=/trunk/; revision=14521

--- calendar/gui/itip-utils.c
+++ calendar/gui/itip-utils.c
@@ -687,7 +687,7 @@ itip_send_comp (CalComponentItipMethod method, CalComponent *send_comp)
 	g_return_if_fail (bonobo_server != NULL);
 	composer_server = BONOBO_OBJREF (bonobo_server);
 
-	GNOME_Evolution_Composer_setMultipartType (composer_server, GNOME_Evolution_Composer_ALTERNATIVE, &ev);
+	GNOME_Evolution_Composer_setMultipartType (composer_server, GNOME_Evolution_Composer_MIXED, &ev);
 	if (BONOBO_EX (&ev)) {		
 		g_warning ("Unable to set multipart type while sending iTip message");
 		goto cleanup;
