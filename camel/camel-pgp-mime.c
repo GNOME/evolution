bad0d146822c285a1120c07cfb067706b73eff18
Jeffrey Stedfast <fejj@ximian.com>
2001-12-14 18:06:16 +0000

    Removed x-inline-pgp-hack kludge because it doesn't work.
    2001-12-14  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* camel-pgp-mime.c (camel_pgp_mime_part_verify): Removed
    	x-inline-pgp-hack kludge because it doesn't work.
    	(camel_pgp_mime_part_decrypt): Same.
    
    svn path=/trunk/; revision=15077

--- camel/camel-pgp-mime.c
+++ camel/camel-pgp-mime.c
@@ -376,30 +376,7 @@ camel_pgp_mime_part_verify (CamelPgpContext *context, CamelMimePart *mime_part, 
 	camel_stream_filter_add (filtered_stream, CAMEL_MIME_FILTER (from_filter));
 	camel_object_unref (CAMEL_OBJECT (from_filter));
 	
-	type = camel_mime_part_get_content_type (mime_part);
-	if (header_content_type_param (type, "x-inline-pgp-hack")) {
-		/* this is a kludge around inline pgp signatures - basically,
-		   the multipart/signed is faked - the original part (aka part #1)
-		   is the original mime part and the signature is a copy of the
-		   signature in part #1 */
-		CamelMimeFilterCharset *charset_filter;
-		CamelContentType *content_type;
-		const char *charset;
-		
-		content_type = camel_mime_part_get_content_type (part);
-		charset = header_content_type_param (content_type, "charset");
-		if (charset) {
-			charset_filter = camel_mime_filter_charset_new_convert ("utf-8", charset);
-			if (charset_filter) {
-				camel_stream_filter_add (filtered_stream,
-							 CAMEL_MIME_FILTER (charset_filter));
-				camel_object_unref (CAMEL_OBJECT (charset_filter));
-			}
-		}
-		
-		wrapper = camel_medium_get_content_object (CAMEL_MEDIUM (part));
-	} else
-		wrapper = CAMEL_DATA_WRAPPER (part);
+	wrapper = CAMEL_DATA_WRAPPER (part);
 	camel_data_wrapper_write_to_stream (wrapper, CAMEL_STREAM (filtered_stream));
 	camel_object_unref (CAMEL_OBJECT (filtered_stream));
 	camel_stream_reset (stream);
@@ -560,27 +537,6 @@ camel_pgp_mime_part_decrypt (CamelPgpContext *context, CamelMimePart *mime_part,
 	
 	camel_object_unref (CAMEL_OBJECT (ciphertext));
 	camel_stream_reset (stream);
-	
-	mime_type = camel_mime_part_get_content_type (mime_part);
-	if (header_content_type_param (mime_type, "x-inline-pgp-hack")) {
-		/* this is a kludge around inline pgp encryption -  basically,
-		   the multipart/encrypted is fake - the original encrypted
-		   content is not really an encrypted mime part so after
-		   decrypting it, we have to fake some mime content headers */
-		CamelStream *hack;
-		
-		hack = camel_stream_mem_new ();
-		
-#define CONTENT_TYPE_TEXT_PLAIN "Content-Type: text/plain\r\n\r\n"
-		camel_stream_write (hack, CONTENT_TYPE_TEXT_PLAIN,
-				    sizeof (CONTENT_TYPE_TEXT_PLAIN) - 1);
-		
-		camel_stream_write_to_stream (stream, hack);
-		camel_stream_reset (hack);
-		
-		camel_object_unref (CAMEL_OBJECT (stream));
-		stream = hack;
-	}
 	
 	/* construct the new decrypted mime part from the stream */
 	part = camel_mime_part_new ();
