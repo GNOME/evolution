ae2d0ca936f9f8995d92bf9553a7750a168a4a95
Jeffrey Stedfast <fejj@ximian.com>
2002-01-02 20:45:42 +0000

    Move the struct into md5-utils.c - this is a kludgey hack around the fact
    2002-01-02  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* md5-utils.h: Move the struct into md5-utils.c - this is a
    	kludgey hack around the fact that NSS also defines
    	MD5Context. Besides, this should be a private struct anyway.
    
    svn path=/trunk/; revision=15227

--- e-util/md5-utils.h
+++ e-util/md5-utils.h
@@ -28,13 +28,8 @@
 
 #include <glib.h>
 
-typedef struct {
-	guint32 buf[4];
-	guint32 bits[2];
-	guchar in[64];
-	gint doByteReverse;
 
-} MD5Context ;
+typedef struct _MD5Context MD5Context;
 
 
 void md5_get_digest (const gchar *buffer, gint buffer_size, guchar digest[16]);
