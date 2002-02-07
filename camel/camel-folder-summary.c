eda028e5a594c85befd8ba29ce42dc1c66cee33e
Jeffrey Stedfast <fejj@ximian.com>
2002-02-07 00:42:30 +0000

    Remove the CAMEL_MESSAGE_NEEDS_REPLY flag, we no longer will be using
    2002-02-06  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* camel-folder-summary.h: Remove the CAMEL_MESSAGE_NEEDS_REPLY
    	flag, we no longer will be using this.
    
    svn path=/trunk/; revision=15586

--- camel/camel-folder-summary.c
+++ camel/camel-folder-summary.c
@@ -2360,7 +2360,6 @@ struct flag_names_t {
 	{ "flagged", CAMEL_MESSAGE_FLAGGED },
 	{ "seen", CAMEL_MESSAGE_SEEN },
 	{ "attachments", CAMEL_MESSAGE_ATTACHMENTS },
-	{ "needsreply", CAMEL_MESSAGE_NEEDS_REPLY },
 	{ NULL, 0 }
 };
 
