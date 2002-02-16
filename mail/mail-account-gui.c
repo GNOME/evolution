40fc52b3220bba58fec4e2abe11105011aca5c6f
Jeffrey Stedfast <fejj@ximian.com>
2002-02-16 00:47:55 +0000

    Ack, strip off the leading '/' char.
    2002-02-15  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* mail-account-gui.c (basename_from_uri): Ack, strip off the
    	leading '/' char.
    
    svn path=/trunk/; revision=15749

--- mail/mail-account-gui.c
+++ mail/mail-account-gui.c
@@ -829,7 +829,7 @@ basename_from_uri (const char *uri)
 	g_assert (base != NULL);
 	
 	/* translate the basename: fixes bug #7160 */
-	return g_strdup (_(base));
+	return g_strdup (_(base + 1));
 }
 
 static void
