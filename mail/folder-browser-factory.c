896cc4eb2292c0d11db7873c7b4fdd642365181e
Jeffrey Stedfast <fejj@src.gnome.org>
2002-09-27 19:23:59 +0000

    added standard header comment containing GPL license info
    svn path=/trunk/; revision=18249

--- mail/folder-browser-factory.c
+++ mail/folder-browser-factory.c
@@ -1,12 +1,25 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- * folder-browser-factory.c: A Bonobo Control factory for Folder Browsers
+ *  Authors: Miguel de Icaza <miguel@ximian.com>
  *
- * Author:
- *   Miguel de Icaza (miguel@ximian.com)
+ *  Copyright 2002 Ximian, Inc. (www.ximian.com)
  *
- * (C) 2000 Ximian, Inc.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *
  */
+
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
