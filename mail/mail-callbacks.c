fb5ac1d550f16611e446a0a3fac3ed491fa7003e
Jeffrey Stedfast <fejj@src.gnome.org>
2003-02-20 22:34:39 +0000

    changed my last fix to use camel_folder_free_uids() - thought I had changed the code to do that (like notzed suggested) before I committed, but apparently didn't?
    svn path=/branches/evolution-1-2-branch/; revision=19984

--- mail/mail-callbacks.c
+++ mail/mail-callbacks.c
@@ -2122,12 +2122,10 @@ mark_all_as_seen (BonoboUIComponent *uih, void *user_data, const char *path)
 	
 	uids = camel_folder_get_uids (fb->folder);
 	camel_folder_freeze (fb->folder);
-	for (i = 0; i < uids->len; i++) {
+	for (i = 0; i < uids->len; i++)
 		camel_folder_set_message_flags (fb->folder, uids->pdata[i], CAMEL_MESSAGE_SEEN, ~0);
-		g_free (uids->pdata[i]);
-	}
+	camel_folder_free_uids (fb->folder, uids);
 	camel_folder_thaw (fb->folder);
-	g_ptr_array_free (uids, TRUE);
 }
 
 void
