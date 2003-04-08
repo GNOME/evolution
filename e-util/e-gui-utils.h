843e045a9cc4f1c177b6f8632eeabc6afb6d9cac
nobody <nobody@localhost>
2003-04-08 21:10:14 +0000

    This commit was manufactured by cvs2svn to create tag 'GAL_1_99_3'.
    svn path=/tags/GAL_1_99_3/; revision=20771

--- e-util/e-gui-utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef E_GUI_UTILS_H
-#define E_GUI_UTILS_H
-
-#include <gtk/gtkwidget.h>
-
-GtkWidget *e_create_image_widget     (gchar *name, gchar *string1, gchar *string2, gint int1, gint int2);
-
-#endif /* E_GUI_UTILS_H */
