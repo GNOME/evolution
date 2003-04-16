d4b1e549f69674ab8c3d36f6ffe2b116269b9032
Ettore Perazzoli <ettore@src.gnome.org>
2003-04-16 14:47:06 +0000

    (remove_bonobo_menus): Only do the actual remove if the ui component actually has a container; this suppresses a BonoboUI warning at each folder view switch.
    svn path=/trunk/; revision=20861

--- widgets/misc/e-search-bar.c
+++ widgets/misc/e-search-bar.c
@@ -564,6 +564,9 @@ setup_bonobo_menus (ESearchBar *esb)
 static void
 remove_bonobo_menus (ESearchBar *esb)
 {
+	if (bonobo_ui_component_get_container (esb->ui_component) == CORBA_OBJECT_NIL)
+		return;
+
 	bonobo_ui_component_rm (esb->ui_component, "/menu/SearchPlaceholder/Search", NULL);
 }
 
