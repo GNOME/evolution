7f7e24e9ba271b18aa0bd87680b9d9c9ec7a750f
nobody <nobody@localhost>
2003-06-28 16:20:00 +0000

    This commit was manufactured by cvs2svn to create tag 'GNOME_UTILS_2_4_0'.
    svn path=/tags/GNOME_UTILS_2_4_0/; revision=21644

--- notes/component-factory.h
+++
@@ -1,7 +0,0 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
-#ifndef COMPONENT_FACTORY_H
-#define COMPONENT_FACTORY_H
-
-void  component_factory_init  (void);
-
-#endif
