1d22c88bd573a61acf5b6bc8dc8ac82e48b41499
nobody <nobody@localhost>
2003-06-28 16:20:00 +0000

    This commit was manufactured by cvs2svn to create tag 'gnome-vfs-volumes-branchpoint'.
    svn path=/tags/gnome-vfs-volumes-branchpoint/; revision=21673

--- addressbook/gui/widgets/e-minicard-control.h
+++
@@ -1,8 +0,0 @@
-#ifndef __E_MINICARD_CONTROL_H__
-#define __E_MINICARD_CONTROL_H__
-
-#include <bonobo/bonobo-control.h>
-
-BonoboControl *e_minicard_control_new (void);
-
-#endif /* __E_MINICARD_CONTROL_H__ */
