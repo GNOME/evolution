ef2bbff26f24acc0f6c8e1473660846922957805
Dan Winship <danw@src.gnome.org>
2003-07-03 12:28:22 +0000

    New function to return an icon for a MIME type, using GnomeIconTheme if
            * e-gui-utils.c (e_icon_for_mime_type): New function to return an
            icon for a MIME type, using GnomeIconTheme if available or
            gnome-vfs and gnome-mime-data if not.
    
    svn path=/trunk/; revision=21733

--- e-util/e-gui-utils.c
+++ e-util/e-gui-utils.c
@@ -11,6 +11,8 @@
  */
 #include <config.h>
 
+#include <string.h>
+
 #include "e-gui-utils.h"
 
 #include <glib.h>
@@ -19,6 +21,14 @@
 #include <gtk/gtkbutton.h>
 #include <gtk/gtklabel.h>
 #include <gtk/gtkhbox.h>
+
+#include <libgnome/gnome-program.h>
+#ifdef HAVE_LIBGNOMEUI_GNOME_ICON_LOOKUP_H
+#include <libgnomeui/gnome-icon-lookup.h>
+#endif
+#include <libgnomevfs/gnome-vfs-mime-handlers.h>
+
+#include "art/empty.xpm"
 
 GtkWidget *e_create_image_widget(gchar *name,
 				 gchar *string1, gchar *string2,
@@ -75,5 +85,86 @@ e_button_new_with_stock_icon (const char *label_str, const char *stockid)
 	gtk_widget_show_all (align);
 
 	return button;
+}
+
+/**
+ * e_icon_for_mime_type:
+ * @mime_type: a MIME type
+ * @size_hint: the size the caller plans to display the icon at
+ *
+ * Tries to find an icon representing @mime_type that will display
+ * nicely at @size_hint by @size_hint pixels. The returned icon
+ * may or may not actually be that size.
+ *
+ * Return value: a pixbuf, which the caller must unref when it is done
+ **/
+GdkPixbuf *
+e_icon_for_mime_type (const char *mime_type, int size_hint)
+{
+	char *icon_name, *icon_path = NULL;
+	GdkPixbuf *pixbuf = NULL;
+
+#ifdef HAVE_LIBGNOMEUI_GNOME_ICON_LOOKUP_H
+	static GnomeIconTheme *icon_theme = NULL;
+
+	/* Try the icon theme. (GNOME 2.2 or Sun GNOME 2.0).
+	 * This will also look in GNOME VFS.
+	 */
+
+	if (!icon_theme)
+		icon_theme = gnome_icon_theme_new ();
+
+	icon_name = gnome_icon_lookup (icon_theme, NULL, NULL, NULL, NULL,
+				       mime_type, 0, NULL);
+	if (icon_name) {
+		icon_path = gnome_icon_theme_lookup_icon (
+			icon_theme, icon_name, size_hint, NULL, NULL);
+		g_free (icon_name);
+	}
+
+#else
+	const char *vfs_icon_name;
+
+	/* Try gnome-vfs. (gnome-vfs.mime itself doesn't offer much,
+	 * but other software packages may define icons for
+	 * themselves.
+	 */
+	vfs_icon_name = gnome_vfs_mime_get_icon (mime_type);
+	if (vfs_icon_name) {
+		icon_path = gnome_program_locate_file (
+			NULL, GNOME_FILE_DOMAIN_PIXMAP,
+			vfs_icon_name, TRUE, NULL);
+	}
+
+	if (!icon_path) {
+		char *p;
+
+		/* Try gnome-mime-data. */
+		icon_name = g_strdup_printf ("document-icons/gnome-%s.png",
+					     mime_type);
+		p = strrchr (icon_name, '/');
+		if (p)
+			*p = '-';
+
+		icon_path = gnome_program_locate_file (
+			NULL, GNOME_FILE_DOMAIN_PIXMAP,
+			icon_name, TRUE, NULL);
+		g_free (icon_name);
+	}
+
+	if (!icon_path) {
+		/* Use the generic document icon. */
+		icon_path = gnome_program_locate_file (NULL, GNOME_FILE_DOMAIN_PIXMAP,
+						      "document-icons/i-regular.png", TRUE, NULL);
+		if (!icon_path) {
+			g_warning ("Could not get any icon for %s!",mime_type);
+			return gdk_pixbuf_new_from_xpm_data((const char **)empty_xpm);
+		}
+	}
+#endif
+
+	pixbuf = gdk_pixbuf_new_from_file (icon_path, NULL);
+	g_free (icon_path);
+	return pixbuf;
 }
 
