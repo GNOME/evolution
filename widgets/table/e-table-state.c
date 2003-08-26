7947421861b484a523f29de10253e50c7375c86e
Harry Lu <harry.lu@sun.com>
2003-08-26 02:47:19 +0000

    ** For bug #47874.
    2003-08-25  Harry Lu  <harry.lu@sun.com>
    
            ** For bug #47874.
    
            * e-table-specification.c (e_table_specification_load_from_file):
            check whether file exists before call xmlParseFile().
            * e-table-state.c (e_table_state_load_from_file): ditto.
            * e-tree-table-adapter.c (open_file): ditto.
    
    svn path=/trunk/; revision=22358

--- widgets/table/e-table-state.c
+++ widgets/table/e-table-state.c
@@ -120,6 +120,10 @@ e_table_state_load_from_file    (ETableState *state,
 				 const char          *filename)
 {
 	xmlDoc *doc;
+
+	if (!g_file_test (filename, G_FILE_TEST_EXISTS))
+		return FALSE;
+
 	doc = xmlParseFile (filename);
 	if (doc) {
 		xmlNode *node = xmlDocGetRootElement(doc);
