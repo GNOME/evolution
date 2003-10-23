ccef3fc1c71d520ee8c14cff5ee3bf1f0ba74401
Rodrigo Moya <rodrigo@ximian.com>
2003-10-23 09:16:41 +0000

    fixed typo in variable name.
    2003-10-23  Rodrigo Moya <rodrigo@ximian.com>
    
    	* gui/component/addressbook-config.c
    	(addressbook_dialog_create_source_table): fixed typo in variable name.
    
    svn path=/trunk/; revision=23028

--- addressbook/gui/component/addressbook-config.c
+++ addressbook/gui/component/addressbook-config.c
@@ -1625,7 +1625,7 @@ addressbook_dialog_create_sources_table (char *name, char *string1, char *string
 	GtkListStore *model;
 
 	scrolled = gtk_scrolled_window_new (NULL, NULL);
-	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scrolld), GTK_SHADOW_IN);
+	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scrolled), GTK_SHADOW_IN);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled),
 					GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 
