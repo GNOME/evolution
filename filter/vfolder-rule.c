9f2fcb717fc961c2f5c3f5abf559a1637206c188
JP Rosevear <jpr@ximian.com>
2003-12-23 14:36:34 +0000

    call peek instead of get as the function was renamed
    2003-12-23  JP Rosevear <jpr@ximian.com>
    
    	* vfolder-rule.c (source_add): call peek instead of get as the
    	function was renamed
    
    svn path=/trunk/; revision=24006

--- filter/vfolder-rule.c
+++ filter/vfolder-rule.c
@@ -454,7 +454,7 @@ source_add(GtkWidget *widget, struct _source_data *data)
 	EMFolderTree *emft;
 	GtkWidget *dialog;
 	
-	emft = (EMFolderTree *) em_folder_tree_new_with_model(mail_component_get_tree_model(mail_component_peek()));
+	emft = (EMFolderTree *) em_folder_tree_new_with_model(mail_component_peek_tree_model(mail_component_peek()));
 	
 	dialog = em_folder_selector_new(emft, EM_FOLDER_SELECTOR_CAN_CREATE, _("Select Folder"), NULL);
 	gtk_window_set_transient_for((GtkWindow *)dialog, (GtkWindow *)gtk_widget_get_toplevel(widget));
