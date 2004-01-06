4fc07cdc605cf61841f40cabdcae3ad6497d964f
Jeffrey Stedfast <fejj@ximian.com>
2004-01-06 15:32:17 +0000

    Protect against a NULL message-info. Afaik, this can only happen if the
    2004-01-06  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* em-message-browser.c (emmb_set_message): Protect against a NULL
    	message-info. Afaik, this can only happen if the message has since
    	been removed? Might be a larger problem here... Anyways, fixes the
    	crash in bug #52297.
    
    svn path=/trunk/; revision=24066

--- mail/em-message-browser.c
+++ mail/em-message-browser.c
@@ -174,9 +174,10 @@ emmb_set_message(EMFolderView *emfv, const char *uid)
 	
 	emmb_parent->set_message(emfv, uid);
 	
-	info = camel_folder_get_message_info (emfv->folder, uid);
-	gtk_window_set_title ((GtkWindow *) emmb->window, camel_message_info_subject (info));
-	camel_folder_free_message_info (emfv->folder, info);
+	if ((info = camel_folder_get_message_info (emfv->folder, uid))) {
+		gtk_window_set_title ((GtkWindow *) emmb->window, camel_message_info_subject (info));
+		camel_folder_free_message_info (emfv->folder, info);
+	}
 	
 	/* Well we don't know if it got displayed (yet) ... but whatever ... */
 	camel_folder_set_message_flags(emfv->folder, uid, CAMEL_MESSAGE_SEEN, CAMEL_MESSAGE_SEEN);
