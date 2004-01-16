c9102b383c917a1dd5ffa202e59ccfe4fe4c3f7d
Jeffrey Stedfast <fejj@ximian.com>
2004-01-16 16:02:43 +0000

    Oops, still need to stat() the src file so we can report what percentage
    2004-01-16  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* em-migrate.c (cp): Oops, still need to stat() the src file so we
    	can report what percentage of the file has been completed so far.
    
    svn path=/trunk/; revision=24274

--- mail/em-migrate.c
+++ mail/em-migrate.c
@@ -1248,10 +1248,13 @@ cp (const char *src, const char *dest, gboolean show_progress)
 	if (stat (dest, &st) == 0 && st.st_size > 0)
 		return -1;
 	
+	if (stat (src, &st) == -1)
+		return -1;
+	
 	if ((fd[0] = open (src, O_RDONLY)) == -1)
 		return -1;
 	
-	if ((fd[1] = open (dest, O_WRONLY | O_CREAT, 0666)) == -1) {
+	if ((fd[1] = open (dest, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
 		errnosav = errno;
 		close (fd[0]);
 		errno = errnosav;
