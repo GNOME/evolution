94f85ffd2262fc7e294e1edc0a569e8e4b855306
Not Zed <NotZed@Ximian.com>
2004-01-23 04:06:50 +0000

    removed the meaningless fixme, a butt-retrieved-number is as good as any
    2004-01-23  Not Zed  <NotZed@Ximian.com>
    
            * providers/imap/camel-imap-folder.c (IMAP_SMALL_BODY_SIZE):
            removed the meaningless fixme, a butt-retrieved-number is as good
            as any in this case.
            (imap_get_message): revert peterw's change of 2002-07-15, instead
            of checking for online mode here, let get_message do it when you
            retrieve the parts.  This lets a multi-fetch (i.e. large) message
            work more betterer in offline mode.
    
    svn path=/trunk/; revision=24376

--- camel/providers/imap/camel-imap-folder.c
+++ camel/providers/imap/camel-imap-folder.c
@@ -1883,7 +1883,6 @@ get_message (CamelImapFolder *imap_folder, const char *uid,
 	return msg;
 }
 
-/* FIXME: I pulled this number out of my butt. */
 #define IMAP_SMALL_BODY_SIZE 5120
 
 static CamelMimeMessage *
@@ -1936,11 +1935,13 @@ imap_get_message (CamelFolder *folder, const char *uid, CamelException *ex)
 	    (stream = camel_imap_folder_fetch_data (imap_folder, uid, "", TRUE, NULL)))
 		return get_message_simple (imap_folder, uid, stream, ex);
 
+#if 0
 	/* If we're not actually connected and it's not in the cache,
 	 * that's as far as we can go.
 	 */
 	if (camel_disco_store_check_online (CAMEL_DISCO_STORE (store), ex) == FALSE)
 		return NULL;
+#endif
 
 	mi = camel_folder_summary_uid (folder->summary, uid);
 	if (mi == NULL) {
