085ce3b2f3662d4ff3a2be8aea629953571b2a8b
Jeffrey Stedfast <fejj@ximian.com>
2004-01-29 01:57:56 +0000

    Same.
    2004-01-28  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* e-search-bar.c (impl_get_property): Same.
    
    	* e-filter-bar.c (get_property): Use
    	g_value_set_string_take_ownership() instead of
    	g_value_take_string() since we need snaps to work with glib-2.0
    	earlier than 2.3.x.
    
    svn path=/trunk/; revision=24507

--- widgets/misc/e-filter-bar.c
+++ widgets/misc/e-filter-bar.c
@@ -550,7 +550,7 @@ get_property (GObject *object, guint property_id, GValue *value, GParamSpec *psp
 			GString *out = g_string_new ("");
 			
 			filter_rule_build_code (efb->current_query, out);
-			g_value_take_string (value, out->str);
+			g_value_set_string_take_ownership (value, out->str);
 			g_string_free (out, FALSE);
 		} else {
 			g_value_set_string (value, NULL);
@@ -595,7 +595,7 @@ get_property (GObject *object, guint property_id, GValue *value, GParamSpec *psp
 		text[n] = '\0';
 		xmlFree (xmlbuf);
 		
-		g_value_take_string (value, text);
+		g_value_set_string_take_ownership (value, text);
 		
 		break; }
 	default:
