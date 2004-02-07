19864e820204ccfb9f25aa7afd82a0be5078d5fe
JP Rosevear <jpr@ximian.com>
2004-02-07 01:25:21 +0000

    use base version for intelligent importers
    2004-02-06  JP Rosevear <jpr@ximian.com>
    
    	* importer/intelligent.c (get_intelligent_importers): use base
    	version for intelligent importers
    
    	* e-shell-importer.c (get_intelligent_importers): ditto
    
    	Fixes #54004
    
    svn path=/trunk/; revision=24666

--- shell/e-shell-importer.c
+++ shell/e-shell-importer.c
@@ -126,6 +126,7 @@ typedef struct _SelectedImporterData{
 } SelectedImporterData;
 
 #define IMPORTER_REPO_ID_QUERY "repo_ids.has ('IDL:GNOME/Evolution/Importer:" BASE_VERSION "')"
+#define IMPORTER_INTEL_REPO_ID_QUERY "repo_ids.has ('IDL:GNOME/Evolution/IntelligentImporter:" BASE_VERSION "')"
 #define IMPORTER_DEBUG
 
 #ifdef IMPORTER_DEBUG
@@ -681,7 +682,7 @@ get_intelligent_importers (void)
 	int i;
 
 	CORBA_exception_init (&ev);
-	info_list = bonobo_activation_query ("repo_ids.has ('IDL:GNOME/Evolution/IntelligentImporter:1.0')", NULL, &ev);
+	info_list = bonobo_activation_query (IMPORTER_INTEL_REPO_ID_QUERY, NULL, &ev);
 	CORBA_exception_free (&ev);
 
 	for (i = 0; i < info_list->_length; i++) {
