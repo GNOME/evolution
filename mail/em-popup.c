c894b82a93f4c20f223ec65f94fcf82bec7d46f9
Jeffrey Stedfast <fejj@ximian.com>
2004-02-11 18:55:13 +0000

    Add license & config.h
    2004-02-11  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* em-popup.c: Add license & config.h
    
    svn path=/trunk/; revision=24709

--- mail/em-popup.c
+++ mail/em-popup.c
@@ -1,6 +1,34 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ *  Authors: Michael Zucchi <notzed@ximian.com>
+ *
+ *  Copyright 2004 Ximian, Inc. (www.ximian.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <string.h>
 #include <stdlib.h>
+
+#include <glib.h>
 
 #include <gtk/gtkmenu.h>
 #include <gtk/gtkmenuitem.h>
@@ -12,8 +40,6 @@
 #include <gtk/gtkimage.h>
 
 #include <libgnome/gnome-url.h>
-
-#include <glib.h>
 
 #include "em-popup.h"
 #include "e-util/e-msgport.h"
