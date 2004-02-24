d729e24f4bcd61b7da2d58dcd03820d8097a1b7a
Jeffrey Stedfast <fejj@ximian.com>
2004-02-24 20:52:44 +0000

    #include <signal.h>, we don't need limits.h
    2004-02-24  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* camel-stream-process.c: #include <signal.h>, we don't need limits.h
    
    svn path=/trunk/; revision=24851

--- camel/camel-stream-process.c
+++ camel/camel-stream-process.c
@@ -27,17 +27,17 @@
 #include <config.h>
 #endif
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <sys/wait.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
 #include <errno.h>
-#include <limits.h>
-#include <stdio.h>
-#include <string.h>
 
 #include "camel-stream-process.h"
 #include "camel-file-utils.h"
