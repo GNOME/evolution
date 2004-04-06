b732ab2d9d5e46ee20657bb891a90c31e4060e89
Not Zed <NotZed@Ximian.com>
2004-04-06 12:11:59 +0000

    #if 0 out the sys/debugreg stuff.
    2004-04-06  Not Zed  <NotZed@Ximian.com>
    
            * camel-debug.c: #if 0 out the sys/debugreg stuff.
    
    svn path=/trunk/; revision=25334

--- camel/camel-debug.c
+++ camel/camel-debug.c
@@ -153,9 +153,9 @@ camel_debug_end(void)
 	pthread_mutex_unlock(&debug_lock);
 }
 
+#if 0
 #include <sys/debugreg.h>
 
-#if 0
 static unsigned
 i386_length_and_rw_bits (int len, enum target_hw_bp_type type)
 {
