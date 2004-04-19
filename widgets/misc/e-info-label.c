f8909cb68587c7adf480b3b6c461f8a37d900946
Jeffrey Stedfast <fejj@ximian.com>
2004-04-19 21:59:36 +0000

    Updated to sue E_ICON_SIZE enums.
    2004-04-19  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* e-info-label.c (e_info_label_new): Updated to sue E_ICON_SIZE enums.
    
    svn path=/trunk/; revision=25532

--- widgets/misc/e-info-label.c
+++ widgets/misc/e-info-label.c
@@ -112,8 +112,8 @@ e_info_label_new(const char *icon)
 {
 	EInfoLabel *el = g_object_new(e_info_label_get_type(), 0);
 	GtkWidget *image;
-	char *name = e_icon_factory_get_icon_filename (icon, 16);
-
+	char *name = e_icon_factory_get_icon_filename (icon, E_ICON_SIZE_MENU);
+	
 	image = gtk_image_new_from_file(name);
 	g_free(name);
 	gtk_misc_set_padding((GtkMisc *)image, 6, 6);
