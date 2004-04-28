b1e8a79cad8a88193a2d258aaf77e51f7dbed7e6
Jeffrey Stedfast <fejj@ximian.com>
2004-04-28 19:13:14 +0000

    Sort the VFolders. Fixes bug #56636.
    2004-04-28  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* em-folder-tree-model.c (sort_cb): Sort the VFolders. Fixes bug
    	#56636.
    
    svn path=/trunk/; revision=25672

--- mail/em-folder-tree-model.c
+++ mail/em-folder-tree-model.c
@@ -198,39 +198,11 @@ sort_cb (GtkTreeModel *model, GtkTreeIter *a, GtkTreeIter *b, gpointer user_data
 		if (!strcmp (bname, _("VFolders")))
 			return -1;
 	} else if (store == vfolder_store) {
-		/* perform no sorting, we want to display in the same
-		 * order as they appear in the VFolder editor - UNMATCHED is always last */
-		GtkTreePath *path;
-		int ret;
-		
+		/* UNMATCHED is always last */
 		if (aname && !strcmp (aname, _("UNMATCHED")))
 			return 1;
 		if (bname && !strcmp (bname, _("UNMATCHED")))
 			return -1;
-		
-		path = gtk_tree_model_get_path (model, a);
-		if (path) {
-			aname = gtk_tree_path_to_string (path);
-			gtk_tree_path_free (path);
-		} else {
-			return 1;
-		}
-		
-		path = gtk_tree_model_get_path (model, b);
-		if (path) {
-			bname = gtk_tree_path_to_string (path);
-			gtk_tree_path_free (path);
-		} else {
-			g_free(aname);
-			return -1;
-		}
-
-		ret = strcmp (aname, bname);
-
-		g_free (aname);
-		g_free (bname);
-		
-		return ret;
 	} else {
 		/* Inbox is always first */
 		if (aname && (!strcmp (aname, "INBOX") || !strcmp (aname, _("Inbox"))))
