0f8af52d9c5e83137c7c80f2b0bca8a60522c83a
Michael Zucci <zucchi@src.gnome.org>
2004-05-12 03:58:08 +0000

    added fixme for stock+label case, which doesn't work.
    svn path=/trunk/; revision=25875

--- widgets/misc/e-error.c
+++ widgets/misc/e-error.c
@@ -408,6 +408,8 @@ e_error_newv(GtkWindow *parent, const char *tag, const char *arg0, va_list ap)
 		for (b = e->buttons;b;b=b->next) {
 			if (b->stock) {
 				if (b->label) {
+					/* FIXME: So although this looks like it will work, it wont.
+					   Need to do it the hard way ... */
 					w = gtk_button_new_from_stock(b->stock);
 					gtk_button_set_label((GtkButton *)w, b->label);
 					gtk_widget_show(w);
