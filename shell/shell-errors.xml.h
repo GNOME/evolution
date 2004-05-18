6163a33d6924d8b6eb1eef15ae8b789486ea8774
Michael Zucci <zucchi@src.gnome.org>
2004-05-18 07:29:26 +0000

    update translation header
    svn path=/trunk/; revision=25952

--- shell/shell-errors.xml.h
+++ shell/shell-errors.xml.h
@@ -13,3 +13,21 @@ char *s = N_("{1}\n"
 	"If you choose to continue, you may not have access to some of your old data.\n"
 	"");
 char *s = N_("Continue");
+/* shell:noshell title */
+char *s = N_("Cannot start Evolution");
+/* shell:noshell primary */
+char *s = N_("Evolution can not start.");
+/* shell:noshell secondary */
+char *s = N_("Your system configuration does not match your Evolution configuration.\n"
+	"\n"
+	"Click help for details");
+/* shell:noshell-reason title */
+char *s = N_("Cannot start Evolution");
+/* shell:noshell-reason primary */
+char *s = N_("Evolution can not start.");
+/* shell:noshell-reason secondary */
+char *s = N_("Your system configuration does not match your Evolution configuration:\n"
+	"\n"
+	"{0}\n"
+	"\n"
+	"Click help for details.");
