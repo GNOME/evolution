dfbf7f2d25a735dbfd5bb9ab9d5fb7a452743c35
JP Rosevear <jpr@novell.com>
2004-05-20 16:31:25 +0000

    only unref the mask if there is one
    2004-05-20  JP Rosevear  <jpr@novell.com>
    
    	* e-task-widget.c (e_task_widget_construct): only unref the mask
    	if there is one
    
    svn path=/branches/evolution-1-5-8/; revision=26013

--- widgets/misc/e-task-widget.c
+++ widgets/misc/e-task-widget.c
@@ -167,7 +167,8 @@ e_task_widget_construct (ETaskWidget *task_widget,
 	gtk_box_pack_start (GTK_BOX (box), priv->label, TRUE, TRUE, 0);
 
 	gdk_pixmap_unref (pixmap);
-	gdk_bitmap_unref (mask);
+	if (mask)
+		gdk_bitmap_unref (mask);
 
 	priv->tooltips = gtk_tooltips_new ();
 	g_object_ref (priv->tooltips);
