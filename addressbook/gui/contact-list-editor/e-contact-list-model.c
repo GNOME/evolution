61423dc27ca64db80aa54b9c965ec2295d945241
Jon Oberheide <jon@focalhost.com>
2004-06-08 03:17:06 +0000

    disable editing of cells in the contact table.
    2004-06-07  Jon Oberheide  <jon@focalhost.com>
    
    	* gui/contact-list-editor/e-contact-list-model.c: disable editing
    	of cells in the contact table.
    
    svn path=/trunk/; revision=26245

--- addressbook/gui/contact-list-editor/e-contact-list-model.c
+++ addressbook/gui/contact-list-editor/e-contact-list-model.c
@@ -44,7 +44,7 @@ contact_list_set_value_at (ETableModel *etc, int col, int row, const void *val)
 static gboolean
 contact_list_is_cell_editable (ETableModel *etc, int col, int row)
 {
-	return TRUE;
+	return FALSE;
 }
 
 /* This function duplicates the value passed to it. */
