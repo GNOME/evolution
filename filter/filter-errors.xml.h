5f0f572bd1e95018fef959ecb63260afa8e6767b
Jeffrey Stedfast <fejj@ximian.com>
2004-06-18 13:24:29 +0000

    Re-added - THIS IS NOT TO BE REMOVED!!!!
    2004-06-18  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* filter-errors.xml.h: Re-added - THIS IS NOT TO BE REMOVED!!!!
    
    svn path=/trunk/; revision=26416

---
+++ filter/filter-errors.xml.h
@@ -0,0 +1,24 @@
+/* filter:no-date primary */
+char *s = N_("Missing date.");
+/* filter:no-date secondary */
+char *s = N_("You must choose a date.");
+/* filter:no-file primary */
+char *s = N_("Missing file name.");
+/* filter:no-file secondary */
+char *s = N_("You must specify a file name.");
+/* filter:bad-file primary */
+char *s = N_("File \"{0}\" does not exist or is not a regular file.");
+/* filter:bad-file secondary */
+char *s = N_("You must specify a file name.");
+/* filter:bad-regexp primary */
+char *s = N_("Bad regular expression \"{0}\".");
+/* filter:bad-regexp secondary */
+char *s = N_("Could not compile regular expression \"{1}\".");
+/* filter:no-name primary */
+char *s = N_("Missing name.");
+/* filter:no-name secondary */
+char *s = N_("You must name this filter.");
+/* filter:bad-name-notunique primary */
+char *s = N_("Name \"{0}\" already used.");
+/* filter:bad-name-notunique secondary */
+char *s = N_("Please choose another name.");
