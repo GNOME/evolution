6883c5737127983d776b7071c258ae74c9bfb4c1
Pamplona Hackers <gnome-desarrollo@es.gnome.org>
2004-09-23 20:59:39 +0000

    Fixes #61978
    2004-09-23  Pamplona Hackers <gnome-desarrollo@es.gnome.org>
    
    	Fixes #61978
    
    	* tools/evolution-addressbook-export.c (main): use more
    	meaningful error message.
    
    svn path=/trunk/; revision=27349

--- addressbook/tools/evolution-addressbook-export.c
+++ addressbook/tools/evolution-addressbook-export.c
@@ -150,7 +150,7 @@ main (int argc, char **argv)
 		action_list_cards_init (&actctx);
 
 	} else {
-		g_warning (_("Impossible internal error."));
+		g_warning (_("Unhandled error"));
 		exit (-1);
 	}
 
