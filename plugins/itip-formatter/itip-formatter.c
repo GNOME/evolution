bb94c723d1f3936e01aa9f7bd806984727369490
JP Rosevear <jpr@novell.com>
2005-01-12 02:01:55 +0000

    Fixes #29985
    2005-01-11  JP Rosevear  <jpr@novell.com>
    
    	Fixes #29985
    
    	* itip-formatter.c (view_response_cb): set the message flags to
    	answered if we send successfully
    
    svn path=/trunk/; revision=28361

--- plugins/itip-formatter/itip-formatter.c
+++ plugins/itip-formatter/itip-formatter.c
@@ -1245,7 +1245,10 @@ view_response_cb (GtkWidget *widget, ItipViewResponse response, gpointer data)
 		}
 		
                 e_cal_component_rescan (comp);
-                itip_send_comp (E_CAL_COMPONENT_METHOD_REPLY, comp, pitip->current_ecal, pitip->top_level, NULL);
+                if (itip_send_comp (E_CAL_COMPONENT_METHOD_REPLY, comp, pitip->current_ecal, pitip->top_level, NULL)) {
+			camel_folder_set_message_flags (((EMFormat *) pitip->pobject.format)->folder, ((EMFormat *) pitip->pobject.format)->uid,
+							CAMEL_MESSAGE_ANSWERED, CAMEL_MESSAGE_ANSWERED);
+		}
 
                 g_object_unref (comp);
 		
