5144eb40ad138ccb2394790d6b3e6707063d3230
Not Zed <NotZed@Ximian.com>
2005-02-23 06:18:53 +0000

    ** See bug #61468
    2005-02-22  Not Zed  <NotZed@Ximian.com>
    
            ** See bug #61468
    
            * providers/imap/camel-imap-folder.c (imap_update_summary): get rid of the stupid
            HEADER.FIELDS NOT() blah, it just caused more problems than it
            ever solved.
    
    svn path=/branches/gnome-2-8/; revision=28861

--- camel/providers/imap/camel-imap-folder.c
+++ camel/providers/imap/camel-imap-folder.c
@@ -2261,7 +2261,7 @@ imap_update_summary (CamelFolder *folder, int exists,
 	
 	CAMEL_SERVICE_ASSERT_LOCKED (store, connect_lock);
 	if (store->server_level >= IMAP_LEVEL_IMAP4REV1)
-		header_spec = "HEADER.FIELDS.NOT (RECEIVED)";
+		header_spec = "HEADER";
 	else
 		header_spec = "0";
 	
