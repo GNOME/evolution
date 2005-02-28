e083da7520bd4cacaf1b19ff2d9483f39abe295f
Not Zed <NotZed@Ximian.com>
2005-02-28 04:31:50 +0000

    reverted Mengjie's patch for 72545.
    2005-02-28  Not Zed  <NotZed@Ximian.com>
    
            * em-folder-browser.c: reverted Mengjie's patch for 72545.
    
    svn path=/trunk/; revision=28910

--- mail/em-folder-browser.c
+++ mail/em-folder-browser.c
@@ -546,15 +546,9 @@ emfb_edit_invert_selection(BonoboUIComponent *uid, void *data, const char *path)
 static void
 emfb_edit_select_all(BonoboUIComponent *uid, void *data, const char *path)
 {
-	EMFolderBrowser *emfb = data;
+	EMFolderView *emfv = data;
 	
-	if (GTK_WIDGET_HAS_FOCUS(((ESearchBar *)emfb->search)->entry))
-		gtk_editable_select_region ((GtkEditable *) (((ESearchBar *)emfb->search)->entry), 0, -1);
-	else if (GTK_WIDGET_HAS_FOCUS(emfb->view.preview->formathtml.html))
-		gtk_html_select_all (emfb->view.preview->formathtml.html);
-	else
-		message_list_select_all(emfb->view.list);
-
+	message_list_select_all(emfv->list);
 }
 
 static void
