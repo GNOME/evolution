a93c01e90efb74c433c883f17bb68f68a632ae90
Chenthill Palanisamy <pchenthill@novell.com>
2005-05-06 07:02:46 +0000

    Fixes #273063
    2005-05-06  Chenthill Palanisamy  <pchenthill@novell.com>
    
    	Fixes #273063
    	* org-gnome-shared-folder-errors.xml:
    	* org-gnome-shared-folder-errors.xml.h:Removed the unwanted
    	tabs and new line characters.
    
    svn path=/trunk/; revision=29300

--- plugins/shared-folder/org-gnome-shared-folder-errors.xml.h
+++ plugins/shared-folder/org-gnome-shared-folder-errors.xml.h
@@ -1,11 +1,8 @@
 /* org.gnome.evolution.mail_shared_folder:invalid-user primary */
 char *s = N_("Invalid user");
 /* org.gnome.evolution.mail_shared_folder:invalid-user secondary */
-char *s = N_("\n"
-	"You cannot share folder with specified user \"{0}\" \n"
-	"");
+char *s = N_("You cannot share folder with specified user \"{0}\"");
 /* org.gnome.evolution.mail_shared_folder:no-user primary */
 char *s = N_("Specify User");
 /* org.gnome.evolution.mail_shared_folder:no-user secondary */
-char *s = N_("\n"
-	"\tYou have to specify a user name whom you want to add to the list \n");
+char *s = N_("You have to specify a user name whom you want to add to the list");
