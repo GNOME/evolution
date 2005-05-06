f77c58f8ae17a65a78213e54f78f8c316ddfc839
Not Zed <NotZed@Ximian.com>
2005-05-06 09:23:04 +0000

    changed .eplug rule to also convert i18n tags if it ends in xml. Also
    2005-05-06  Not Zed  <NotZed@Ximian.com>
    
            * plugin.mk: changed .eplug rule to also convert i18n tags if it
            ends in xml.  Also convert .error.xml into .error converting i18n
            tags.
    
    svn path=/trunk/; revision=29303

--- plugin.mk
+++ plugin.mk
@@ -1,2 +1,9 @@
 %.eplug: %.eplug.in
 	sed -e 's|\@PLUGINDIR\@|$(plugindir)|' $< > $@
+
+%.eplug: %.eplug.xml
+	LC_ALL=C $(INTLTOOL_MERGE) -x -u /tmp $< - | \
+	sed -e 's|\@PLUGINDIR\@|$(plugindir)|' > $@
+
+%.error: %.error.xml
+	LC_ALL=C $(INTLTOOL_MERGE) -x -u /tmp $< $@
