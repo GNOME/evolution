ce5a2a93db68558e34f360c71e7e770699bb25c7
Michael Zucci <zucchi@src.gnome.org>
2005-05-11 07:22:53 +0000

    fixed the rule for .eplug.xml to avoid stupid intltool-merge diagnostics going into the file
    svn path=/trunk/; revision=29322

--- plugin.mk
+++ plugin.mk
@@ -1,9 +1,8 @@
 %.eplug: %.eplug.in
 	sed -e 's|\@PLUGINDIR\@|$(plugindir)|' $< > $@
 
-%.eplug: %.eplug.xml
-	LC_ALL=C $(INTLTOOL_MERGE) -x -u /tmp $< - | \
-	sed -e 's|\@PLUGINDIR\@|$(plugindir)|' > $@
+%.eplug.in: %.eplug.xml
+	LC_ALL=C $(INTLTOOL_MERGE) -x -u /tmp $< $@
 
 %.error: %.error.xml
 	LC_ALL=C $(INTLTOOL_MERGE) -x -u /tmp $< $@
