76e5ee9c55d6b7c40b7a823b85babb8988b82a72
nobody <nobody@localhost>
2005-06-01 03:56:13 +0000

    This commit was manufactured by cvs2svn to create tag 'GAL_2_5_3'.
    svn path=/tags/GAL_2_5_3/; revision=29437

--- plugin.mk
+++
@@ -1,8 +0,0 @@
-%.eplug: %.eplug.in
-	sed -e 's|\@PLUGINDIR\@|$(plugindir)|' $< > $@
-
-%.eplug.in: %.eplug.xml
-	LC_ALL=C $(INTLTOOL_MERGE) -x -u /tmp $< $@
-
-%.error: %.error.xml
-	LC_ALL=C $(INTLTOOL_MERGE) -x -u /tmp $< $@
