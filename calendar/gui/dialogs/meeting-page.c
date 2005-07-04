2511473c17a7d5502c359c4f457faf65e45c2569
Srinivasa Ragavan <sragavan@novell.com>
2005-07-04 06:05:01 +0000

    Added vertical scrollbar policy of list view to always so that it does not
    2005-07-04  Srinivasa Ragavan <sragavan@novell.com>
    
    	* gui/dialogs/meeting-page.c: (meeting_page_construct) Added
    	vertical scrollbar policy of list view to always so that it does
    	not extend the view when there are more contacts.
    
    svn path=/trunk/; revision=29623

--- calendar/gui/dialogs/meeting-page.c
+++ calendar/gui/dialogs/meeting-page.c
@@ -1025,7 +1025,7 @@ meeting_page_construct (MeetingPage *mpage, EMeetingStore *ems,
 
 	gtk_widget_show (GTK_WIDGET (priv->list_view));
 	sw = gtk_scrolled_window_new (NULL, NULL);
-	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (sw), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (sw), GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
 	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (sw), GTK_SHADOW_IN);
 	gtk_widget_show (sw);
 	gtk_container_add (GTK_CONTAINER (sw), GTK_WIDGET (priv->list_view));
