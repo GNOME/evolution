0679612173857a8339065dff0c2d02cdea57c12d
Shreyas Srinivasan <sshreyas@novell.com>
2005-07-29 06:16:15 +0000

    Fix #307358. Check if the provider type exists before senisitizing
    2005-07-29  Shreyas Srinivasan  <sshreyas@novell.com>
    
    	* em-account-editor.c (emae_defaults_page): Fix #307358. Check if
    	the provider type exists  before senisitizing widgets.
    
    svn path=/trunk/; revision=29929

--- mail/em-account-editor.c
+++ mail/em-account-editor.c
@@ -2141,11 +2141,14 @@ emae_defaults_page(EConfig *ec, EConfigItem *item, struct _GtkWidget *parent, st
 	emae_account_entry(emae, "bcc_addrs", E_ACCOUNT_BCC_ADDRS, xml);
 
 	gtk_widget_set_sensitive((GtkWidget *)gui->drafts_folder_button, e_account_writable(emae->account, E_ACCOUNT_DRAFTS_FOLDER_URI));
-	gtk_widget_set_sensitive((GtkWidget *)gui->sent_folder_button, e_account_writable(emae->account, E_ACCOUNT_SENT_FOLDER_URI)&& !(emae->priv->source.provider->flags & CAMEL_PROVIDER_DISABLE_SENT_FOLDER));
+
+	gtk_widget_set_sensitive((GtkWidget *)gui->sent_folder_button, e_account_writable(emae->account, E_ACCOUNT_SENT_FOLDER_URI)  
+				 && ( emae->priv->source.provider && !(emae->priv->source.provider->flags & CAMEL_PROVIDER_DISABLE_SENT_FOLDER)));
+
 	gtk_widget_set_sensitive((GtkWidget *)gui->restore_folders_button,
 				 (e_account_writable(emae->account, E_ACCOUNT_SENT_FOLDER_URI) &&
-				   ! ( emae->priv->source.provider->flags & CAMEL_PROVIDER_DISABLE_SENT_FOLDER))
-				 || e_account_writable(emae->account, E_ACCOUNT_DRAFTS_FOLDER_URI));
+				  ( emae->priv->source.provider && ! ( emae->priv->source.provider->flags & CAMEL_PROVIDER_DISABLE_SENT_FOLDER)) || 
+				  e_account_writable(emae->account, E_ACCOUNT_DRAFTS_FOLDER_URI)));
 
 	/* Receipt policy */
 	emae_setup_receipt_policy (emae, xml);
