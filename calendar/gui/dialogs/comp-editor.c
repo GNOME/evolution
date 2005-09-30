b7d9892cae363764e0f418feb4c0fedc347a0d9d
Chenthill Palanisamy <pchen@src.gnome.org>
2005-09-30 11:26:55 +0000

    Fixes #273847.
    svn path=/trunk/; revision=30439

--- calendar/gui/dialogs/comp-editor.c
+++ calendar/gui/dialogs/comp-editor.c
@@ -2422,7 +2422,7 @@ comp_editor_get_mime_attach_list (CompEditor *editor)
 		CamelDataWrapper *wrapper;
 		CamelStreamMem *mstream;
 		unsigned char *buffer = NULL;
-		const char *desc;
+		const char *desc, *disp;
 
 		cal_mime_attach = g_malloc0 (sizeof (struct CalMimeAttach));
 		wrapper = camel_medium_get_content_object (CAMEL_MEDIUM (l->data));
@@ -2441,6 +2441,10 @@ comp_editor_get_mime_attach_list (CompEditor *editor)
 		cal_mime_attach->description = g_strdup (desc);
 		cal_mime_attach->content_type = g_strdup (camel_data_wrapper_get_mime_type (wrapper));
 		
+		disp = camel_mime_part_get_disposition ((CamelMimePart *)l->data);
+		if (disp && !g_ascii_strcasecmp(disp, "inline"))	
+			cal_mime_attach->disposition = TRUE;
+			
 		attach_list = g_slist_append (attach_list, cal_mime_attach);
 
 		camel_object_unref (mstream);
