9340aaef8974cd2b6143fc6b8bdaf16d5007c571
Veerapuram Varadhan <vvaradan@src.gnome.org>
2005-12-20 10:33:29 +0000

    ** See Bug #246233 : Changed "Search Editor" to "Searches"
            ** See Bug #246233
            * e-filter-bar.c: (member_activated) : Changed "Search Editor" to
            "Searches"
    
    svn path=/trunk/; revision=30902

--- widgets/misc/e-filter-bar.c
+++ widgets/misc/e-filter-bar.c
@@ -200,7 +200,7 @@ menubar_activated (ESearchBar *esb, int id, void *data)
 		if (!efb->save_dialog) {
 			efb->save_dialog = dialog = (GtkWidget *) rule_editor_new (efb->context, FILTER_SOURCE_INCOMING, _("_Searches"));
 			
-			gtk_window_set_title (GTK_WINDOW (dialog), _("Search Editor"));
+			gtk_window_set_title (GTK_WINDOW (dialog), _("Searches"));
 			g_signal_connect (dialog, "response", G_CALLBACK (full_rule_editor_response), efb);
 			g_object_weak_ref ((GObject *) dialog, (GWeakNotify) rule_editor_destroyed, efb);
 			gtk_widget_show (dialog);
