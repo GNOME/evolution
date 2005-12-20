2e5070b5672eb705f83ccee85d8e95ea19240bdd
Andre Klapper <a9016009@gmx.de>
2005-12-20 17:33:35 +0000

    updated Evolution release number in unstable warning popup
    2005-12-20  Andre Klapper <a9016009@gmx.de>
    	* main.c: updated Evolution release number in unstable warning popup
    
    svn path=/trunk/; revision=30907

--- shell/main.c
+++ shell/main.c
@@ -242,7 +242,7 @@ show_development_warning(void)
                   "\n"
 		  "We hope that you enjoy the results of our hard work, and we\n"
 		  "eagerly await your contributions!\n"),
-		"2.4.1");
+		"2.4.2.1");
 	label = gtk_label_new (text);
 	g_free(text);
 
