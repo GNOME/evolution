634b570468a3b56df0a631ce6497a0595ce3ec42
Andre Klapper <a9016009@gmx.de>
2006-01-21 11:48:51 +0000

    changed string "Settings" to "Preferences" to keep consistency. Fixes bug
    2006-01-21  Andre Klapper <a9016009@gmx.de>
    
    	* e-shell-settings-dialog.c: changed string "Settings" to
    	"Preferences" to keep consistency. Fixes bug 327933.
    
    svn path=/trunk/; revision=31258

--- shell/e-shell-settings-dialog.c
+++ shell/e-shell-settings-dialog.c
@@ -315,7 +315,7 @@ e_shell_settings_dialog_init (EShellSettingsDialog *dialog)
 	load_pages (dialog);
 	set_dialog_size (dialog);
 	
-	gtk_window_set_title (GTK_WINDOW (dialog), _("Evolution Settings"));
+	gtk_window_set_title (GTK_WINDOW (dialog), _("Evolution Preferences"));
 	gtk_dialog_set_has_separator (GTK_DIALOG (dialog), FALSE);
 }
 
