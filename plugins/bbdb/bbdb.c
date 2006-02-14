b7287fc33da0e38518d8f118dbed927b2b4c3cff
Andre Klapper <a9016009@gmx.de>
2006-02-14 14:37:16 +0000

    Another typo fix from gfree to g_free.
    2006-02-14  Andre Klapper <a9016009@gmx.de>
    	* bbdb.c (bbdb_page_factory): Another typo fix from gfree to g_free.
    
    svn path=/trunk/; revision=31521

--- plugins/bbdb/bbdb.c
+++ plugins/bbdb/bbdb.c
@@ -474,7 +474,7 @@ bbdb_page_factory (EPlugin *ep, EConfigHookItemFactoryData *hook_data)
 	frame_label = gtk_label_new ("");
 	str = g_strdup_printf ("<span weight=\"bold\">%s</span>", _("Instant Messaging Contacts"));
 	gtk_label_set_markup (GTK_LABEL (frame_label), str);
-	gfree (str);
+	g_free (str);
 	GTK_MISC (frame_label)->xalign = 0.0;
 	gtk_box_pack_start (GTK_BOX (frame), frame_label, FALSE, FALSE, 0);
 
