8c867d4bf6e4450f4ce804ccd33fecf94d577d11
Simon Zheng <simon.zheng@sun.com>
2006-03-21 03:11:34 +0000

    Fixes bug #326571 Add em_filename_make_safe(), and use it to allow
    2006-03-21  Simon Zheng  <simon.zheng@sun.com>
    
            Fixes bug #326571
            * em-utils.c (em_filename_make_safe), (emu_get_save_filesel),
            (emu_save_parts_response):
            Add em_filename_make_safe(), and use it to allow filenames
            contain more valid characters when saving attachment.
    
    svn path=/branches/gnome-2-14/; revision=31727

--- mail/em-utils.c
+++ mail/em-utils.c
@@ -322,6 +322,40 @@ em_utils_edit_filters (GtkWidget *parent)
 	gtk_widget_show (GTK_WIDGET (filter_editor));
 }
 
+/* 
+ * Picked this from e-d-s/libedataserver/e-data. 
+ * But it allows more characters to occur in filenames, especially when saving attachment.
+ */
+void
+em_filename_make_safe (gchar *string)
+{
+	gchar *p, *ts;
+	gunichar c;
+#ifdef G_OS_WIN32
+	const char *unsafe_chars = "/\":*?<>|\\";
+#else
+	const char *unsafe_chars = "/";
+#endif	
+	
+	g_return_if_fail (string != NULL);
+	p = string;
+
+	while(p && *p) {
+		c = g_utf8_get_char (p);
+		ts = p;
+		p = g_utf8_next_char (p);
+		/* I wonder what this code is supposed to actually
+		 * achieve, and whether it does that as currently
+		 * written?
+		 */
+		if (!g_unichar_isprint(c) || ( c < 0xff && strchr (unsafe_chars, c&0xff ))) {
+			while (ts<p) 	
+				*ts++ = '_';
+		}
+	}
+}
+
+
 /* Saving messages... */
 
 static GtkWidget *
@@ -358,7 +392,7 @@ emu_get_save_filesel (GtkWidget *parent, const char *title, const char *name, Gt
 
 	if (name && name[0]) {
 		realname = g_strdup (name);
-		e_filename_make_safe (realname);
+		em_filename_make_safe (realname);
 	} else {
 		realname = NULL;
 	}
@@ -502,7 +536,7 @@ emu_save_parts_response (GtkWidget *filesel, int response, GSList *parts)
                                 }
                         } else {
 				safe_name = g_strdup(file_name);
-				e_filename_make_safe(safe_name);
+				em_filename_make_safe(safe_name);
 				file_name = safe_name;
 			}
 			
