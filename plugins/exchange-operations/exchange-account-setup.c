47c520fc84fb2d020ed618ee3928225cfd29c66c
Sushma Rai <rsushma@src.gnome.org>
2006-05-10 10:16:22 +0000

    Not showing the worng folder size 0 KB in offline mode. See #339266.
    svn path=/branches/gnome-2-14/; revision=31967

--- plugins/exchange-operations/exchange-account-setup.c
+++ plugins/exchange-operations/exchange-account-setup.c
@@ -928,6 +928,7 @@ org_gnome_exchange_show_folder_size_factory (EPlugin *epl, EConfigHookItemFactor
 	GtkVBox *vbx;
 	GtkHBox *hbx_size;
 	char *folder_name, *folder_size;
+	int mode;
 
 	service = CAMEL_SERVICE (camel_folder_get_parent_store (cml_folder));
 	if (!service)
@@ -939,11 +940,16 @@ org_gnome_exchange_show_folder_size_factory (EPlugin *epl, EConfigHookItemFactor
 
 	if (g_ascii_strcasecmp (provider->protocol, "exchange"))
 		return NULL;
+
+	account = exchange_operations_get_exchange_account ();
+	exchange_account_is_offline (account, &mode);
+	if (mode == OFFLINE_MODE)
+		return NULL;
 	
 	folder_name = (char*) camel_folder_get_name (cml_folder);
 	if (!folder_name)
 		folder_name = g_strdup ("name");
-	account = exchange_operations_get_exchange_account ();
+
 	model = exchange_account_folder_size_get_model (account);
 	if (model)
 		folder_size = g_strdup_printf ("%s KB", exchange_folder_size_get_val (model, folder_name));
