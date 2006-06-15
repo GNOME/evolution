52f6398aa703b882c17fecb3203a0c124f11a5b0
Tor Lillqvist <tml@novell.com>
2006-06-15 10:11:41 +0000

    Must expand also @GETTEXT_PACKAGE@ and @LOCALEDIR@, at least the caldav
    2006-06-15  Tor Lillqvist  <tml@novell.com>
    
    	* plugin.mk: Must expand also @GETTEXT_PACKAGE@ and @LOCALEDIR@,
    	at least the caldav plugin has a .eplug.in file that refers to
    	those. (On the other hand, it is questionable why
    	org-gnome-evolution-caldav.eplug.in needs to specify domain and
    	localedir at all.)
    
    svn path=/branches/gnome-2-14/; revision=32147

--- plugin.mk
+++ plugin.mk
@@ -1,5 +1,8 @@
 %.eplug: %.eplug.in
-	sed -e 's|\@PLUGINDIR\@|$(plugindir)|' -e 's|\@SOEXT\@|$(SOEXT)|' $< > $@
+	sed -e 's|\@PLUGINDIR\@|$(plugindir)|'		\
+	-e 's|\@SOEXT\@|$(SOEXT)|'			\
+	-e 's|\@GETTEXT_PACKAGE\@|$(GETTEXT_PACKAGE)|'	\
+	-e 's|\@LOCALEDIR\@|$(localedir)|' $< > $@
 
 %.eplug.in: %.eplug.xml
 	LC_ALL=C $(INTLTOOL_MERGE) -x -u /tmp $< $@
