b860ef8e1fb5d33b364c3332dfa91d9f499d5764
Yuri Pankov <yuri.pankov@gmail.com>
2006-07-22 10:51:18 +0000

    Add missing headers. Fixes #344744.
    2006-07-22  Yuri Pankov <yuri.pankov@gmail.com>
    
    	* message-list.c: Add missing headers. Fixes #344744.
    
    svn path=/trunk/; revision=32382

--- mail/message-list.c
+++ mail/message-list.c
@@ -56,11 +56,13 @@
 #include "misc/e-gui-utils.h"
 
 #include "table/e-cell-checkbox.h"
+#include "table/e-cell-hbox.h"
 #include "table/e-cell-date.h"
 #include "table/e-cell-size.h"
 #include "table/e-cell-text.h"
 #include "table/e-cell-toggle.h"
 #include "table/e-cell-tree.h"
+#include "table/e-cell-vbox.h"
 #include "table/e-tree-memory-callbacks.h"
 #include "table/e-tree-memory.h"
 
