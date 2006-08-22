68d581d982c23a71fac38342835b2cf902771241
Srinivasa Ragavan <sragavan@src.gnome.org>
2006-08-22 17:56:43 +0000

    ** Fix for bug #350337
    svn path=/trunk/; revision=32633

--- mail/message-list.c
+++ mail/message-list.c
@@ -1407,8 +1407,8 @@ ml_tree_value_at (ETreeModel *etm, ETreePath path, int col, void *model_data)
 	case COL_MIXED_SENDER:		
 	case COL_SENDER:{
 		char **sender_name = NULL;
-        	str = camel_message_info_from (msg_info);
-	        if(str!=""){
+		str = camel_message_info_from (msg_info);
+		if(str && str[0] != '\0'){
 			sender_name=g_strsplit(str,"<",2);
 			return (void *)(*sender_name);
 		}
