f14ab0afa345a49816beb215a1b3e2042c13ec40
Srinivasa Ragavan <sragavan@src.gnome.org>
2006-08-23 06:14:03 +0000

    ** Fix for bug #350395 by Ãystein GisnÃ¥s
    svn path=/trunk/; revision=32635

--- plugins/itip-formatter/itip-view.c
+++ plugins/itip-formatter/itip-view.c
@@ -150,7 +150,8 @@ format_date_and_time_x		(struct tm	*date_tm,
 	
 	/* Calculate a normalized "tomorrow" */
 	tomorrow_tm = *current_tm;
-	if (tomorrow_tm.tm_mday == time_days_in_month (date_tm->tm_year + 1900, date_tm->tm_mon)) {
+	/* Don't need this if date is in the past. Also, year assumption won't fail. */
+	if (date_tm->tm_year >= current_tm->tm_year && tomorrow_tm.tm_mday == time_days_in_month (date_tm->tm_year + 1900, date_tm->tm_mon)) {
 		tomorrow_tm.tm_mday = 1;
 		if (tomorrow_tm.tm_mon == 11) {
 			tomorrow_tm.tm_mon = 1;
@@ -164,7 +165,8 @@ format_date_and_time_x		(struct tm	*date_tm,
 
 	/* Calculate a normalized "next seven days" */
 	week_tm = *current_tm;
-	if (week_tm.tm_mday + 6 > time_days_in_month (date_tm->tm_year + 1900, date_tm->tm_mon)) {
+	/* Don't need this if date is in the past. Also, year assumption won't fail. */
+	if (date_tm->tm_year >= current_tm->tm_year && week_tm.tm_mday + 6 > time_days_in_month (date_tm->tm_year + 1900, date_tm->tm_mon)) {
 		week_tm.tm_mday = (week_tm.tm_mday + 6) % time_days_in_month (date_tm->tm_year + 1900, date_tm->tm_mon);
 		if (week_tm.tm_mon == 11) {
 			week_tm.tm_mon = 1;
