b67fa40029cb940798f08e6d2cba8d74a0d62f08
Matthew Barnes <mbarnes@redhat.com>
2006-09-18 17:21:36 +0000

    Fixes #351332
    2006-09-18  Matthew Barnes  <mbarnes@redhat.com>
    
    	Fixes #351332
    
    	* e-msg-composer.c: Fix implicit function declarations.
    
    svn path=/trunk/; revision=32790

--- composer/e-msg-composer.c
+++ composer/e-msg-composer.c
@@ -62,6 +62,7 @@
 #include <gconf/gconf-client.h>
 
 #include <libgnome/gnome-exec.h>
+#include <libgnome/gnome-help.h>
 #include <libgnome/gnome-i18n.h>
 #include <libgnomeui/gnome-uidefs.h>
 #include <libgnomeui/gnome-window-icon.h>
