7ef3a9a585a1525774c3e71540adfe1eb0a53205
Harish Krishnaswamy <kharish@novell.com>
2006-10-16 18:46:54 +0000

    Update last stable version.
    2006-10-17  Harish Krishnaswamy  <kharish@novell.com>
    
    	* main.c: (show_development_warning):  Update last
    	stable version.
    
    svn path=/trunk/; revision=32903

--- shell/main.c
+++ shell/main.c
@@ -245,7 +245,7 @@ show_development_warning(void)
                   "\n"
 		  "We hope that you enjoy the results of our hard work, and we\n"
 		  "eagerly await your contributions!\n"),
-		"2.8.0");
+		"2.8.1");
 	label = gtk_label_new (text);
 	g_free(text);
 
