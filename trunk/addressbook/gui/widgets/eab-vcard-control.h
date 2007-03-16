649671169033854bf0a44753d86ba88277a21dc3
Harish Krishnaswamy <kharish@src.gnome.org>
2007-03-16 09:23:46 +0000

    Ouch..setting right the blooper of adding trunk inside the branch
    svn path=/branches/gnome-2-18/; revision=33314

--- trunk/addressbook/gui/widgets/eab-vcard-control.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EAB_VCARD_CONTROL_H__
-#define __EAB_VCARD_CONTROL_H__
-
-#include <bonobo/bonobo-control.h>
-
-BonoboControl *eab_vcard_control_new (void);
-
-#endif /* __EAB_VCARD_CONTROL_H__ */
--- trunk/plugins/exchange-operations/exchange-folder-subscription.h
+++
@@ -1,7 +0,0 @@
-#ifndef __EXCHANGE_FOLDER_SUBSCRIPTION_H__
-#define __EXCHANGE_FOLDER_SUBSCRIPTION_H__
-
-gboolean
-create_folder_subscription_dialog (ExchangeAccount *account, gchar *fname);
-
-#endif
