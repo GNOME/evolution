c70d6dfd3d4f06e34c9d596102727940105472d1
Matthew Barnes <mbarnes@redhat.com>
2007-05-01 17:50:54 +0000

    Bump stable version recommendation from 2.8.1 to 2.10.1.
    2007-05-01  Matthew Barnes  <mbarnes@redhat.com>
    
    	* shell/main.c (show_development_warning):
    	Bump stable version recommendation from 2.8.1 to 2.10.1.
    
    
    svn path=/trunk/; revision=33468

--- shell/main.c
+++ shell/main.c
@@ -231,7 +231,7 @@ show_development_warning(void)
                   "\n"
 		  "We hope that you enjoy the results of our hard work, and we\n"
 		  "eagerly await your contributions!\n"),
-		"2.8.1");
+		"2.10.1");
 	label = gtk_label_new (text);
 	g_free(text);
 
