2248b0052a671be1f4bd85fcb9f5e3d12c0e2237
Srinivasa Ragavan <sragavan@src.gnome.org>
2007-06-15 08:41:40 +0000

    ** Fix for bug #446870
    svn path=/trunk/; revision=33674

--- e-util/e-gui-utils.c
+++ e-util/e-gui-utils.c
@@ -61,3 +61,29 @@ e_icon_for_mime_type (const char *mime_type, int size_hint)
 
 	return pixbuf;
 }
+
+GtkWidget *e_create_image_widget(gchar *name,
+				 gchar *string1, gchar *string2,
+				 gint int1, gint int2)
+{
+	GtkWidget *alignment = NULL;
+	GtkWidget *w;
+	
+	if (string1) {
+		w = e_icon_factory_get_image (string1, E_ICON_SIZE_DIALOG);
+
+		gtk_misc_set_alignment (GTK_MISC (w), 0.5, 0.5);
+
+		alignment = gtk_widget_new(gtk_alignment_get_type(),
+					   "child", w,
+					   "xalign", (double) 0,
+					   "yalign", (double) 0,
+					   "xscale", (double) 0,
+					   "yscale", (double) 0,
+					   NULL);
+
+		gtk_widget_show_all (alignment);
+	}
+
+	return alignment;
+}
--- e-util/e-gui-utils.h
+++ e-util/e-gui-utils.h
@@ -4,5 +4,6 @@
 #include <gtk/gtkwidget.h>
 
 GdkPixbuf *e_icon_for_mime_type          (const char *mime_type, int size);
+GtkWidget *e_create_image_widget         (gchar *name, gchar *string1, gchar *string2, gint int1, gint int2);
 
 #endif /* E_GUI_UTILS_H */
