2ab08e462db3a123f03e5ae778e6328446d40220
Srinivasa Ragavan <sragavan@src.gnome.org>
2007-09-03 07:57:04 +0000

    Fix a build break.
    svn path=/trunk/; revision=34165

--- calendar/conduits/common/libecalendar-common-conduit.h
+++ calendar/conduits/common/libecalendar-common-conduit.h
@@ -8,13 +8,4 @@
 int e_pilot_add_category_if_possible(char *cat_to_add, struct CategoryAppInfo *category);
 void e_pilot_local_category_to_remote(int * pilotCategory, ECalComponent *comp, struct CategoryAppInfo *category);
 void e_pilot_remote_category_to_local(int   pilotCategory, ECalComponent *comp, struct CategoryAppInfo *category);
-#include <libedataserver/e-categories.h>
-#include <libecal/e-cal.h>
-#include <pi-appinfo.h>
 
-#define PILOT_MAX_CATEGORIES 16
-
-
-int e_pilot_add_category_if_possible(char *cat_to_add, struct CategoryAppInfo *category);
-void e_pilot_local_category_to_remote(int * pilotCategory, ECalComponent *comp, struct CategoryAppInfo *category);
-void e_pilot_remote_category_to_local(int   pilotCategory, ECalComponent *comp, struct CategoryAppInfo *category);
