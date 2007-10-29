1c749ee5c6d6b6cf8c962385e503c2a50c93bc3d
Srinivasa Ragavan <sragavan@novell.com>
2007-10-29 07:19:25 +0000

    Remove the version. Update the stable version.
    2007-10-29  Srinivasa Ragavan  <sragavan@novell.com>
    
    	* GNOME_Evolution_Shell.server.in.in: Remove the version.
    	* main.c: (show_development_warning): Update the stable version.
    
    
    svn path=/trunk/; revision=34444

--- shell/main.c
+++ shell/main.c
@@ -231,7 +231,7 @@ show_development_warning(void)
                   "\n"
 		  "We hope that you enjoy the results of our hard work, and we\n"
 		  "eagerly await your contributions!\n"),
-		"2.10.1");
+		"2.12.1");
 	label = gtk_label_new (text);
 	g_free(text);
 
