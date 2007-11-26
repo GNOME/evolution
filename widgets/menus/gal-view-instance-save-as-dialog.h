dd34eba30cd90659975dc8b56d00dfe36f243854
Gilles Dartiguelongue <gdartigu@svn.gnome.org>
2007-11-26 21:40:20 +0000

    ** Fixes bug #495951 Use GtkTreeView in place of ETable
    2007-11-26  Gilles Dartiguelongue  <gdartigu@svn.gnome.org>
    
            ** Fixes bug #495951
            Use GtkTreeView in place of ETable
    
    
    svn path=/trunk/; revision=34590

--- widgets/menus/gal-view-instance-save-as-dialog.h
+++ widgets/menus/gal-view-instance-save-as-dialog.h
@@ -26,7 +26,8 @@
 
 #include <gtk/gtkdialog.h>
 #include <glade/glade.h>
-#include <table/e-table-model.h>
+#include <gtk/gtktreeview.h>
+#include <gtk/gtktreemodel.h>
 #include <widgets/menus/gal-view-collection.h>
 #include <widgets/menus/gal-view-instance.h>
 
@@ -63,7 +64,11 @@ struct _GalViewInstanceSaveAsDialog
 
 	/* item specific fields */
 	GladeXML *gui;
-	ETableModel *model;
+	GtkTreeView *treeview;
+	GtkTreeModel *model;
+
+	GtkWidget *scrolledwindow, *radiobutton_replace;
+	GtkWidget *entry_create, *radiobutton_create;
 
 	GalViewInstance *instance;
 	GalViewCollection *collection;
