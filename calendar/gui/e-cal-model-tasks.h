ee4335fd753c100631d6104743ed1040939cc5c9
Milan Crha <mcrha@redhat.com>
2008-01-28 17:53:07 +0000

    ** Fix for bug #240073
    2008-01-28  Milan Crha  <mcrha@redhat.com>
    
    	** Fix for bug #240073
    
    	* gui/e-cal-model-tasks.h: (enum ECalModelTasksField):
    	Move strikeout field to known fields, otherwise the "Click to add" row
    	will have text strikeouted.
    
    
    svn path=/trunk/; revision=34920

--- calendar/gui/e-cal-model-tasks.h
+++ calendar/gui/e-cal-model-tasks.h
@@ -46,8 +46,8 @@ typedef enum {
 	E_CAL_MODEL_TASKS_FIELD_PRIORITY,
 	E_CAL_MODEL_TASKS_FIELD_STATUS,
 	E_CAL_MODEL_TASKS_FIELD_URL,
-	E_CAL_MODEL_TASKS_FIELD_LAST,
-	E_CAL_MODEL_TASKS_FIELD_STRIKEOUT /* it's another virtual readonly column */
+	E_CAL_MODEL_TASKS_FIELD_STRIKEOUT, /* it's another virtual readonly column */
+	E_CAL_MODEL_TASKS_FIELD_LAST
 } ECalModelTasksField;
 
 typedef struct {
