5ce421aecb9210e2e494b0503ffd986836373138
Matthew Barnes <mbarnes@redhat.com>
2008-03-27 17:20:21 +0000

    ** Fixes bug #523271
    2008-03-27  Matthew Barnes  <mbarnes@redhat.com>
    
    	** Fixes bug #523271
    
    	* mail/em-popup.c (emp_standard_menu_factory):
    	Lowercase the MIME type before passing it to
    	gnome_vfs_mime_get_all_applications().
    
    
    svn path=/trunk/; revision=35276

--- mail/em-popup.c
+++ mail/em-popup.c
@@ -788,6 +788,12 @@ emp_standard_menu_factory(EPopup *emp, void *data)
 	}
 
 	if (mime_type) {
+                gchar *cp;
+
+                /* GNOME-VFS expects lowercase MIME types. */
+                for (cp = mime_type; *cp != '\0'; cp++)
+                        *cp = g_ascii_tolower (*cp);
+
 		apps = gnome_vfs_mime_get_all_applications(mime_type);
 
 		if (apps == NULL && strcmp(mime_type, "application/octet-stream") == 0) {
