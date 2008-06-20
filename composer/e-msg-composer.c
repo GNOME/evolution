8652b4f84ce12f91138bf6eba33ec63f3dd4dfa6
Jeffrey Stedfast <fejj@src.gnome.org>
2008-06-20 22:12:01 +0000

    fixed copyright notices
    svn path=/trunk/; revision=35662

--- composer/e-msg-composer.c
+++ composer/e-msg-composer.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
 /* e-msg-composer.c
  *
- * Copyright (C) 1999-2003 Ximian, Inc. (www.ximian.com)
+ * Copyright (C) 1999-2008 Novell, Inc. (www.novell.com)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General Public
@@ -43,15 +43,15 @@
 
 #define SMIME_SUPPORTED 1
 
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include <glib.h>
 
