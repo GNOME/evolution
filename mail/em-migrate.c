9d89b214fd7b6a6b60ea1a808d472b783a30618a
Jeff Cai <jeff.cai@sun.com>
2008-07-28 03:19:05 +0000

    Use G_GNUC_PRETTY_FUNCTION instead of __PRETTY__FUNCTION
    2008-07-28 Jeff Cai <jeff.cai@sun.com> 
    
        * em-migrate.c: (migrate_to_db):
        * em-network-prefs.c: (notify_proxy_type_changed),
        (widget_entry_changed_cb):
        Use G_GNUC_PRETTY_FUNCTION instead of __PRETTY__FUNCTION
    
    
    svn path=/trunk/; revision=35844

--- mail/em-migrate.c
+++ mail/em-migrate.c
@@ -2934,7 +2934,7 @@ migrate_to_db()
 				migrate_folders(store, info, account->name, &ex);
 				
 			} else
-				printf("%s(%d):%s: failed to get folder infos \n", __FILE__, __LINE__, __PRETTY_FUNCTION__);
+				printf("%s(%d):%s: failed to get folder infos \n", __FILE__, __LINE__, G_GNUC_PRETTY_FUNCTION);
 			camel_exception_clear(&ex);
 				
 		}
