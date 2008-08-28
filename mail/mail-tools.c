cdb84aceeedbdddd057226fee6d16452c48ee820
Sankarasivasubramanian Pasupathilingam <psankar@src.gnome.org>
2008-08-28 07:59:47 +0000

    CamelStore Goodness. Folder->store thing is yet to begin.
    svn path=/branches/mail-dbus-remoting/; revision=36149

--- mail/mail-tools.c
+++ mail/mail-tools.c
@@ -91,7 +91,7 @@ mail_tool_get_trash (const gchar *url, int connect, CamelException *ex)
 	if (!store)
 		return NULL;
 
-	if (connect || ((CamelService *) store)->status == CAMEL_SERVICE_CONNECTED)
+	if (connect || (camel_store_remote_get_status (store) == CAMEL_SERVICE_CONNECTED))
 		trash = camel_store_get_trash_remote (store, ex);
 	else
 		trash = NULL;
