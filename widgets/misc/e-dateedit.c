d636d0ead7923279e711e65c8cf8ad4061e6c522
Sankarasivasubramanian Pasupathilingam <psankar@src.gnome.org>
2008-10-17 16:46:27 +0000

    License changes.
    svn path=/trunk/; revision=36641

--- widgets/misc/e-dateedit.c
+++ widgets/misc/e-dateedit.c
@@ -1,23 +1,21 @@
 /*
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) version 3.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
+ * You should have received a copy of the GNU Library General Public
  * License along with the program; if not, see <http://www.gnu.org/licenses/>  
- *
  *
  * Authors:
  *		Damon Chaplin <damon@ximian.com>
  *
  * Copyright (C) 1999-2008 Novell, Inc. (www.novell.com)
- *
  */
 
 /*
