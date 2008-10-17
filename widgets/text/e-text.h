3ca238674e7b6c647f225e745f739b6acc0f9445
Sankarasivasubramanian Pasupathilingam <psankar@src.gnome.org>
2008-10-17 16:48:02 +0000

    License changes.
    svn path=/branches/gnome-2-24/; revision=36642

--- widgets/text/e-text.h
+++ widgets/text/e-text.h
@@ -2,9 +2,9 @@
  * e-text.h - Text item for evolution.
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) version 3.
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
