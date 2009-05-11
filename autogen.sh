b7fbe1d689879c7c1639129b8ebe518a29242477
Damien Lespiau <damien.lespiau@intel.com>
2009-05-11 09:59:57 -0400

    Bug 582168 – Remove duplicated shebang and comment from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-#!/bin/sh
-# Run this to generate all the initial makefiles, etc.
-
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
