461eb9e6aa231edc12686b94a46b9fb52e04f020
H.Habighorst <tearofadragon@googlemail.com>
2009-06-23 11:17:39 -0400

    Bug 586632 – Require automake 1.9

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="Evolution"
-REQUIRED_AUTOMAKE_VERSION=1.6
+REQUIRED_AUTOMAKE_VERSION=1.9
 
 (test -f $srcdir/configure.ac \
   && test -f $srcdir/ChangeLog \
