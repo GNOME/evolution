704ee96aa27011109be2b282517abc153657edaf
Milan Crha <mcrha@redhat.com>
2009-07-27 17:06:20 +0200

    Bug #579646 - Remember alarm's last notified per calendar

--- calendar/gui/alarm-notify/config-data.h
+++ calendar/gui/alarm-notify/config-data.h
@@ -28,6 +28,7 @@
 
 #include <glib.h>
 #include <libical/ical.h>
+#include <libecal/e-cal.h>
 #include <gconf/gconf-client.h>
 #include <libedataserver/e-source-list.h>
 
@@ -36,8 +37,8 @@ GConfClient  *config_data_get_conf_client (void);
 icaltimezone *config_data_get_timezone (void);
 gboolean      config_data_get_24_hour_format (void);
 gboolean      config_data_get_notify_with_tray (void);
-void          config_data_set_last_notification_time (time_t t);
-time_t        config_data_get_last_notification_time (void);
+void          config_data_set_last_notification_time (ECal *cal, time_t t);
+time_t        config_data_get_last_notification_time (ECal *cal);
 void          config_data_save_blessed_program (const gchar *program);
 gboolean      config_data_is_blessed_program (const gchar *program);
 ESourceList  *config_data_get_calendars (const gchar *);
