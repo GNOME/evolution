6fced70ee07656daf73bf13cab658bf4c2a6baac
Milan Crha <mcrha@redhat.com>
2009-10-02 17:22:59 +0200

    No IDL from eds and server.mk file in evo sources any more
    Fixes build with latest eds master (after dbus-cal merge).

--- server.mk
+++
@@ -1,9 +0,0 @@
-%.server.in: %.server.in.in $(top_builddir)/config.h
-	sed -e 's|\@BINDIR_IN_SERVER_FILE\@|$(bindir_in_server_file)|'	\
-	-e 's|\@PRIVLIBEXECDIR_IN_SERVER_FILE\@|$(privlibexecdir_in_server_file)|'	\
-	-e 's|\@COMPONENTDIR_IN_SERVER_FILE\@|$(componentdir_in_server_file)|'	\
-	-e 's|\@VERSION\@|$(BASE_VERSION)|' 			\
-	-e 's|\@EXEEXT\@|$(EXEEXT)|'				\
-	-e 's|\@SOEXT\@|$(SOEXT)|'				\
-	-e 's|\@INTERFACE_VERSION\@|$(INTERFACE_VERSION)|' $< > $@
-
