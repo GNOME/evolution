6660aa57d943066bdd14751dae8a26e8a2cd32dd
Srinivasa Ragavan <sragavan@gnome.org>
2009-11-04 14:29:07 +0000

    Fix a compiler error

--- mail/mail-ops.c
+++ mail/mail-ops.c
@@ -623,7 +623,7 @@ mail_send_message(CamelFolder *queue, const gchar *uid, const gchar *destination
 	if (!camel_exception_is_set(ex)) {
 		camel_folder_set_message_flags (queue, uid, CAMEL_MESSAGE_DELETED|CAMEL_MESSAGE_SEEN, ~0);
 		/* Sync it to disk, since if it crashes in between, we keep sending it again on next start. */
-		camel_folder_sync (queue, FALSE);
+		camel_folder_sync (queue, FALSE, NULL);
 	}
 
 	if (err->len) {
