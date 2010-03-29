b947206eed3d00573bf6577d60a5f53bf5fe89e7
Michael Meeks <michael.meeks@novell.com>
2010-03-29 09:49:53 +0100

    Last scroll-pane, and manual sizing tweak

--- addressbook/gui/contact-editor/e-contact-editor.c
+++ addressbook/gui/contact-editor/e-contact-editor.c
@@ -2670,7 +2670,7 @@ init_all (EContactEditor *editor)
 		e_builder_get_widget (editor->builder, "vbox-size-leader"), &tab_req);
 	gtk_widget_set_size_request (
 		e_builder_get_widget (editor->builder, "scrolledwindow-size-leader"),
-		tab_req.width, tab_req.height);
+		tab_req.width, tab_req.height + 8);
 }
 
 static void
