c209ecc49865fc3f4d5574e6e5f8585baf12795f
Chenthill Palanisamy <pchenthill@novell.com>
2010-06-10 12:15:14 +0530

    Check for gtk version below 2.21.1 for comptability with gseal changes

--- e-util/gtk-compat.h
+++ e-util/gtk-compat.h
@@ -4,10 +4,11 @@
 #include <gtk/gtk.h>
 
 /* Provide a compatibility layer for accessor functions introduced
- * in GTK+ 2.22 which we need to build with sealed GDK.  That way it
+ * in GTK+ 2.21.1 which we need to build with sealed GDK.  That way it
  * is still possible to build with GTK+ 2.20. */
 
-#if !GTK_CHECK_VERSION(2,21,2)
+#if (GTK_MAJOR_VERSION == 2 && GTK_MINOR_VERSION < 21) \
+	|| (GTK_MINOR_VERSION == 21 && GTK_MICRO_VERSION < 1)
 
 #define gdk_drag_context_get_actions(context)		(context)->actions
 #define gdk_drag_context_get_suggested_action(context)	(context)->suggested_action
