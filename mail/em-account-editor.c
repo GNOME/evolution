ed144aaf1b82440dbff01e461520132046299565
Srinivasa Ragavan <sragavan@gnome.org>
2010-06-30 13:17:45 +0530

    MeeGo Bugzilla #3286 - Allow edit accounts in the capplet to go fwd/back.

--- mail/em-account-editor.c
+++ mail/em-account-editor.c
@@ -2168,8 +2168,8 @@ emae_identity_page (EConfig *ec, EConfigItem *item, GtkWidget *parent, GtkWidget
 	GtkWidget *w;
 	GtkBuilder *builder;
 
-	/*if (old)
-	  return old;*/
+	if (old && emae->type == EMAE_PAGES)
+	  return old;
 
 	account = em_account_editor_get_modified_account (emae);
 
