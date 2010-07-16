8b5dc5fcb1ab4b9cf6fcb4dbeda41ece0c52c49e
David Woodhouse <David.Woodhouse@intel.com>
2010-07-16 07:18:46 +0100

    Add translation note for 'Group Reply'.

--- mail/e-mail-reader.c
+++ mail/e-mail-reader.c
@@ -2853,8 +2853,11 @@ e_mail_reader_init (EMailReader *reader)
 
 	/* Likewise the "mail-reply-group" action */
 
-	menu_tool_action = e_menu_tool_action_new (
-		"mail-reply-group", _("Group Reply"),
+        /* For Translators: "Group Reply" will reply either to a mailing list
+	   (if possible and if that configuration option is enabled), or else
+	   it will reply to all. The word "Group" was chosen because it covers
+	   either of those, without too strongly implying one or the other. */
+	menu_tool_action = e_menu_tool_action_new ("mail-reply-group", _("Group Reply"),
 		_("Reply to the mailing list, or to all recipients"), NULL);
 
 	gtk_action_set_icon_name (
