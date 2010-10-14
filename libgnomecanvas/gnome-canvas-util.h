b27cf6c033befb99677d75970cf3011bb0873624
Benjamin Otte <otte@redhat.com>
2010-10-15 01:51:45 +0200

    gnome-canvas: Remove now-unused functions

--- libgnomecanvas/gnome-canvas-util.h
+++ libgnomecanvas/gnome-canvas-util.h
@@ -35,58 +35,7 @@
 
 #include <libgnomecanvas/gnome-canvas.h>
 
-#include <libart_lgpl/art_svp.h>
-#include <libart_lgpl/art_vpath.h>
-#include <libart_lgpl/art_svp_vpath_stroke.h>
-
 G_BEGIN_DECLS
-
-/* Given three points forming an angle, compute the coordinates of the inside and outside points of
- * the mitered corner formed by a line of a given width at that angle.
- *
- * If the angle is less than 11 degrees, then FALSE is returned and the return points are not
- * modified.  Otherwise, TRUE is returned.
- */
-gint gnome_canvas_get_miter_points (gdouble x1, gdouble y1, gdouble x2, gdouble y2, gdouble x3, gdouble y3,
-				   gdouble width,
-				   gdouble *mx1, gdouble *my1, gdouble *mx2, gdouble *my2);
-
-/* Compute the butt points of a line segment.  If project is FALSE, then the results are as follows:
- *
- *            -------------------* (bx1, by1)
- *                               |
- *   (x1, y1) *------------------* (x2, y2)
- *                               |
- *            -------------------* (bx2, by2)
- *
- * that is, the line is not projected beyond (x2, y2).  If project is TRUE, then the results are as
- * follows:
- *
- *            -------------------* (bx1, by1)
- *                      (x2, y2) |
- *   (x1, y1) *-------------*    |
- *                               |
- *            -------------------* (bx2, by2)
- */
-void gnome_canvas_get_butt_points (gdouble x1, gdouble y1, gdouble x2, gdouble y2,
-				   gdouble width, gint project,
-				   gdouble *bx1, gdouble *by1, gdouble *bx2, gdouble *by2);
-
-/* Calculate the distance from a polygon to a point.  The polygon's X coordinates are in the even
- * indices of the poly array, and the Y coordinates are in the odd indices.
- */
-gdouble gnome_canvas_polygon_to_point (gdouble *poly, gint num_points, gdouble x, gdouble y);
-
-/* Sets the svp to the new value, requesting repaint on what's changed. This function takes responsibility for
- * freeing new_svp.
- */
-void gnome_canvas_update_svp (GnomeCanvas *canvas, ArtSVP **p_svp, ArtSVP *new_svp);
-
-/* Sets the svp to the new value, clipping if necessary, and requesting repaint
- * on what's changed. This function takes responsibility for freeing new_svp.
- */
-void gnome_canvas_update_svp_clip (GnomeCanvas *canvas, ArtSVP **p_svp, ArtSVP *new_svp,
-				   ArtSVP *clip_svp);
 
 /* Sets the svp to the new value, requesting repaint on what's changed. This
  * function takes responsibility for freeing new_svp. This routine also adds the
@@ -94,30 +43,8 @@ void gnome_canvas_update_svp_clip (GnomeCanvas *canvas, ArtSVP **p_svp, ArtSVP *
  */
 void gnome_canvas_item_reset_bounds (GnomeCanvasItem *item);
 
-/* Sets the svp to the new value, requesting repaint on what's changed. This function takes responsibility for
- * freeing new_svp. This routine also adds the svp's bbox to the item's.
- */
-void gnome_canvas_item_update_svp (GnomeCanvasItem *item, ArtSVP **p_svp, ArtSVP *new_svp);
-
-/* Sets the svp to the new value, clipping if necessary, and requesting repaint
- * on what's changed. This function takes responsibility for freeing new_svp.
- */
-void gnome_canvas_item_update_svp_clip (GnomeCanvasItem *item, ArtSVP **p_svp, ArtSVP *new_svp,
-					ArtSVP *clip_svp);
-
-/* Request redraw of the svp if in aa mode, or the entire item in in xlib
- * mode.
- */
-void gnome_canvas_item_request_redraw_svp (GnomeCanvasItem *item, const ArtSVP *svp);
-
 /* Sets the bbox to the new value, requesting full repaint. */
 void gnome_canvas_update_bbox (GnomeCanvasItem *item, gint x1, gint y1, gint x2, gint y2);
-
-/* Convert from GDK line join specifier to libart. */
-ArtPathStrokeJoinType gnome_canvas_join_gdk_to_art (GdkJoinStyle gdk_join);
-
-/* Convert from GDK line cap specifier to libart. */
-ArtPathStrokeCapType gnome_canvas_cap_gdk_to_art (GdkCapStyle gdk_cap);
 
 /* Create a scratch cairo_t for measuring purposes */
 cairo_t *gnome_canvas_cairo_create_scratch (void);
