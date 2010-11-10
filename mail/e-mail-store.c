1d8d943f67a4e52d8c574e2598f0f2d7e03e488d
Milan Crha <mcrha@redhat.com>
2010-11-10 11:40:43 +0100

    Bug #632293 - Do not add POP accounts into folder tree

--- mail/e-mail-store.c
+++ mail/e-mail-store.c
@@ -171,6 +171,9 @@ mail_store_add (EMailSession *session,
 	StoreInfo *store_info;
 
 	g_return_if_fail (store_table != NULL);
+	g_return_if_fail (store != NULL);
+	g_return_if_fail (CAMEL_IS_STORE (store));
+	g_return_if_fail ((CAMEL_SERVICE (store)->provider->flags & CAMEL_PROVIDER_IS_STORAGE) != 0);
 
 	default_model = em_folder_tree_model_get_default ();
 	folder_cache = e_mail_session_get_folder_cache (session);
