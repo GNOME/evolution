5f92f064e670b784dbc78d789e85e21fb83d3559
Philip Withnall <philip@tecnocode.co.uk>
2011-06-14 09:21:27 +0200

    Bug #652394 - Port google-account-setup plugin to libgdata 0.9.0

--- plugins/google-account-setup/google-source.c
+++ plugins/google-account-setup/google-source.c
@@ -418,6 +418,9 @@ static void
 retrieve_list_clicked (GtkButton *button, GtkComboBox *combo)
 {
 	ESource *source;
+	#ifdef HAVE_LIBGDATA_0_9
+	GDataClientLoginAuthorizer *authorizer;
+	#endif
 	GDataCalendarService *service;
 	GDataFeed *feed;
 	gchar *user, *password, *tmp;
@@ -448,8 +451,14 @@ retrieve_list_clicked (GtkButton *button, GtkComboBox *combo)
 		return;
 	}
 
+	#ifdef HAVE_LIBGDATA_0_9
+	authorizer = gdata_client_login_authorizer_new ("evolution-client-0.1.0", GDATA_TYPE_CALENDAR_SERVICE);
+	service = gdata_calendar_service_new (GDATA_AUTHORIZER (authorizer));
+	if (!gdata_client_login_authorizer_authenticate (authorizer, user, password, NULL, &error)) {
+	#else
 	service = gdata_calendar_service_new ("evolution-client-0.1.0");
 	if (!gdata_service_authenticate (GDATA_SERVICE (service), user, password, NULL, &error)) {
+	#endif
 		/* Error! */
 		claim_error (parent, error->message);
 		g_error_free (error);
@@ -559,6 +568,9 @@ retrieve_list_clicked (GtkButton *button, GtkComboBox *combo)
 	}
 
 	g_object_unref (service);
+	#ifdef HAVE_LIBGDATA_0_9
+	g_object_unref (authorizer);
+	#endif
 	g_free (user);
 }
 
