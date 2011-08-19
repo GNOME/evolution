c58254deff5d9ce793fceb3aa24643ee5298b876
Andre Klapper <a9016009@gmx.de>
2011-08-19 15:30:50 +0200

    Capitalize tab header as all others are capitalized too

--- plugins/itip-formatter/itip-formatter.c
+++ plugins/itip-formatter/itip-formatter.c
@@ -2989,7 +2989,7 @@ itip_formatter_page_factory (EPlugin *ep, EConfigHookItemFactoryData *hook_data)
 	/* Create a new notebook page */
 	page = gtk_vbox_new (FALSE, 0);
 	gtk_container_set_border_width (GTK_CONTAINER (page), 12);
-	tab_label = gtk_label_new (_("Meeting invitations"));
+	tab_label = gtk_label_new (_("Meeting Invitations"));
 	gtk_notebook_append_page (GTK_NOTEBOOK (hook_data->parent), page, tab_label);
 
 	/* Frame */
