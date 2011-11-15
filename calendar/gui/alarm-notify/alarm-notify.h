5fc51b95fe98fb7a08afe58e81837d3ec09fa763
Milan Crha <mcrha@redhat.com>
2011-11-15 13:51:01 +0100

    Bug #664016 - [evolution-alarm-notify] Try reconnect offline calendars

--- calendar/gui/alarm-notify/alarm-notify.h
+++ calendar/gui/alarm-notify/alarm-notify.h
@@ -69,8 +69,7 @@ AlarmNotify *	alarm_notify_new		(GCancellable *cancellable,
 						 GError **error);
 void		alarm_notify_add_calendar	(AlarmNotify *an,
 						 ECalClientSourceType source_type,
-						 ESource *source,
-						 gboolean load_afterwards);
+						 ESource *source);
 void		alarm_notify_remove_calendar	(AlarmNotify *an,
 						 ECalClientSourceType source_type,
 						 const gchar *str_uri);
