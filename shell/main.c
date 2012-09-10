a276c7ce42292e05963a878cc799e92e78fc7e7b
Milan Crha <mcrha@redhat.com>
2012-09-10 10:40:43 +0200

    Bug #683548 - Initialize webkit's web view before other threads are run
    Just a workaround until Webkit upstream has that fixed.

--- shell/main.c
+++ shell/main.c
@@ -53,6 +53,8 @@
 
 #include <libedataserverui/libedataserverui.h>
 
+#include <webkit/webkit.h>
+
 #include "e-shell.h"
 #include "e-shell-migrate.h"
 #include "e-shell-meego.h"
@@ -425,6 +427,17 @@ create_default_shell (void)
 	return shell;
 }
 
+/* this workarounds https://bugzilla.gnome.org/show_bug.cgi?id=683548
+   and is a replacement of g_type_ensure(), which is part of GLib 2.34,
+   on which Evolution doesn't depend yet
+*/
+static void
+e_ensure_type (GType type)
+{
+	if (G_UNLIKELY (type == (GType) -1))
+		g_warning ("can't happen");
+}
+
 gint
 main (gint argc,
       gchar **argv)
@@ -624,6 +637,10 @@ main (gint argc,
 
 	g_object_unref (settings);
 #endif
+
+	/* Workaround https://bugzilla.gnome.org/show_bug.cgi?id=683548 */
+	if (!quit)
+		e_ensure_type (WEBKIT_TYPE_WEB_VIEW);
 
 	shell = create_default_shell ();
 
