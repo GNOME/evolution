3964b272b70e87bf93bd26a1d2303496ea0b2b6c
Matthew Barnes <mbarnes@redhat.com>
2012-10-09 13:03:47 -0400

    ECalConfigDateEdit: Use G_DEFINE_DYNAMIC_TYPE.
    Follow the usual GObject conventions.
    
    (cherry picked from commit 6fd48fd4df20ebf76382ebb441e81e85be7e3719)

--- modules/calendar/e-cal-config-date-edit.h
+++ modules/calendar/e-cal-config-date-edit.h
@@ -19,12 +19,47 @@
 #ifndef E_CAL_CONFIG_DATE_EDIT_H
 #define E_CAL_CONFIG_DATE_EDIT_H
 
-#include <glib-object.h>
+#include <libebackend/libebackend.h>
+
+/* Standard GObject macros */
+#define E_TYPE_CAL_CONFIG_DATE_EDIT \
+	(e_cal_config_date_edit_get_type ())
+#define E_CAL_CONFIG_DATE_EDIT(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST \
+	((obj), E_TYPE_CAL_CONFIG_DATE_EDIT, ECalConfigDateEdit))
+#define E_CAL_CONFIG_DATE_EDIT_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_CAST \
+	((cls), E_TYPE_CAL_CONFIG_DATE_EDIT, ECalConfigDateEditClass))
+#define E_IS_CAL_CONFIG_DATE_EDIT(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE \
+	((obj), E_TYPE_CAL_CONFIG_DATE_EDIT))
+#define E_IS_CAL_CONFIG_DATE_EDIT_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_TYPE \
+	((cls), E_TYPE_CAL_CONFIG_DATE_EDIT))
+#define E_CAL_CONFIG_DATE_EDIT_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS \
+	((obj), E_TYPE_CAL_CONFIG_DATE_EDIT, ECalConfigDateEditClass))
 
 G_BEGIN_DECLS
 
-void e_cal_config_date_edit_register_type (GTypeModule *type_module);
+typedef struct _ECalConfigDateEdit ECalConfigDateEdit;
+typedef struct _ECalConfigDateEditClass ECalConfigDateEditClass;
+typedef struct _ECalConfigDateEditPrivate ECalConfigDateEditPrivate;
+
+struct _ECalConfigDateEdit {
+	EExtension parent;
+	ECalConfigDateEditPrivate *priv;
+};
+
+struct _ECalConfigDateEditClass {
+	EExtensionClass parent_class;
+};
+
+GType		e_cal_config_date_edit_get_type	(void) G_GNUC_CONST;
+void		e_cal_config_date_edit_type_register
+						(GTypeModule *type_module);
 
 G_END_DECLS
 
 #endif /* E_CAL_CONFIG_DATE_EDIT_H */
+
