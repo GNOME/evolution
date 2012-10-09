6fd48fd4df20ebf76382ebb441e81e85be7e3719
Matthew Barnes <mbarnes@redhat.com>
2012-10-09 13:03:47 -0400

    ECalConfigDateEdit: Use G_DEFINE_DYNAMIC_TYPE.
    Follow the usual GObject conventions.

--- modules/calendar/evolution-module-calendar.c
+++ modules/calendar/evolution-module-calendar.c
@@ -84,7 +84,7 @@ e_module_load (GTypeModule *type_module)
 
 	e_cal_config_calendar_item_type_register (type_module);
 	e_cal_config_comp_editor_type_register (type_module);
-	e_cal_config_date_edit_register_type (type_module);
+	e_cal_config_date_edit_type_register (type_module);
 	e_cal_config_meeting_store_register_type (type_module);
 	e_cal_config_meeting_time_selector_register_type (type_module);
 	e_cal_config_model_register_type (type_module);
