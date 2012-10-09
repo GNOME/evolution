1889a59ae90663481709429f0813206263761ed2
Matthew Barnes <mbarnes@redhat.com>
2012-10-09 13:35:08 -0400

    ECalConfigMeetingTimeSelector: Use G_DEFINE_DYNAMIC_TYPE.
    Follow the usual GObject conventions.
    
    (cherry picked from commit 51e192f235cda7d70d76974ac4522280264fd011)

--- modules/calendar/e-cal-config-meeting-time-selector.h
+++ modules/calendar/e-cal-config-meeting-time-selector.h
@@ -19,12 +19,48 @@
 #ifndef E_CAL_CONFIG_MEETING_TIME_SELECTOR_H
 #define E_CAL_CONFIG_MEETING_TIME_SELECTOR_H
 
-#include <glib-object.h>
+#include <libebackend/libebackend.h>
+
+/* Standard GObject macros */
+#define E_TYPE_CAL_CONFIG_MEETING_TIME_SELECTOR \
+	(e_cal_config_meeting_time_selector_get_type ())
+#define E_CAL_CONFIG_MEETING_TIME_SELECTOR(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST \
+	((obj), E_TYPE_CAL_CONFIG_MEETING_TIME_SELECTOR, ECalConfigMeetingTimeSelector))
+#define E_CAL_CONFIG_MEETING_TIME_SELECTOR_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_CAST \
+	((cls), E_TYPE_CAL_CONFIG_MEETING_TIME_SELECTOR, ECalConfigMeetingTimeSelectorClass))
+#define E_IS_CAL_CONFIG_MEETING_TIME_SELECTOR(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE \
+	((obj), E_TYPE_CAL_CONFIG_MEETING_TIME_SELECTOR))
+#define E_IS_CAL_CONFIG_MEETING_TIME_SELECTOR_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_TYPE \
+	((cls), E_TYPE_CAL_CONFIG_MEETING_TIME_SELECTOR))
+#define E_CAL_CONFIG_MEETING_TIME_SELECTOR_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS \
+	((obj), E_TYPE_CAL_CONFIG_MEETING_TIME_SELECTOR, ECalConfigMeetingTimeSelectorClass))
 
 G_BEGIN_DECLS
 
-void e_cal_config_meeting_time_selector_register_type (GTypeModule *type_module);
+typedef struct _ECalConfigMeetingTimeSelector ECalConfigMeetingTimeSelector;
+typedef struct _ECalConfigMeetingTimeSelectorClass ECalConfigMeetingTimeSelectorClass;
+typedef struct _ECalConfigMeetingTimeSelectorPrivate ECalConfigMeetingTimeSelectorPrivate;
+
+struct _ECalConfigMeetingTimeSelector {
+	EExtension parent;
+	ECalConfigMeetingTimeSelectorPrivate *priv;
+};
+
+struct _ECalConfigMeetingTimeSelectorClass {
+	EExtensionClass parent_class;
+};
+
+GType		e_cal_config_meeting_time_selector_get_type
+						(void) G_GNUC_CONST;
+void		e_cal_config_meeting_time_selector_type_register
+						(GTypeModule *type_module);
 
 G_END_DECLS
 
 #endif /* E_CAL_CONFIG_MEETING_TIME_SELECTOR_H */
+
--- modules/calendar/evolution-module-calendar.c
+++ modules/calendar/evolution-module-calendar.c
@@ -86,7 +86,7 @@ e_module_load (GTypeModule *type_module)
 	e_cal_config_comp_editor_type_register (type_module);
 	e_cal_config_date_edit_type_register (type_module);
 	e_cal_config_meeting_store_type_register (type_module);
-	e_cal_config_meeting_time_selector_register_type (type_module);
+	e_cal_config_meeting_time_selector_type_register (type_module);
 	e_cal_config_model_register_type (type_module);
 	e_cal_config_view_register_type (type_module);
 
