27e8442447fccab49ffbf3be3d2d520bfbd5f6ba
Matthew Barnes <mbarnes@redhat.com>
2012-10-09 13:20:44 -0400

    ECalConfigMeetingStore: Use G_DEFINE_DYNAMIC_TYPE.
    Follow the usual GObject conventions.

--- modules/calendar/e-cal-config-meeting-store.h
+++ modules/calendar/e-cal-config-meeting-store.h
@@ -19,11 +19,46 @@
 #ifndef E_CAL_CONFIG_MEETING_STORE_H
 #define E_CAL_CONFIG_MEETING_STORE_H
 
-#include <glib-object.h>
+#include <libebackend/libebackend.h>
+
+/* Standard GObject macros */
+#define E_TYPE_CAL_CONFIG_MEETING_STORE \
+	(e_cal_config_meeting_store_get_type ())
+#define E_CAL_CONFIG_MEETING_STORE(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST \
+	((obj), E_TYPE_CAL_CONFIG_MEETING_STORE, ECalConfigMeetingStore))
+#define E_CAL_CONFIG_MEETING_STORE_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_CAST \
+	((cls), E_TYPE_CAL_CONFIG_MEETING_STORE, ECalConfigMeetingStoreClass))
+#define E_IS_CAL_CONFIG_MEETING_STORE(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE \
+	((obj), E_TYPE_CAL_CONFIG_MEETING_STORE))
+#define E_IS_CAL_CONFIG_MEETING_STORE_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_TYPE \
+	((cls), E_TYPE_CAL_CONFIG_MEETING_STORE))
+#define E_CAL_CONFIG_MEETING_STORE_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS \
+	((obj), E_TYPE_CaL_CONFIG_MEETING_STORE, ECalConfigMeetingStoreClass))
 
 G_BEGIN_DECLS
 
-void e_cal_config_meeting_store_register_type (GTypeModule *type_module);
+typedef struct _ECalConfigMeetingStore ECalConfigMeetingStore;
+typedef struct _ECalConfigMeetingStoreClass ECalConfigMeetingStoreClass;
+typedef struct _ECalConfigMeetingStorePrivate ECalConfigMeetingStorePrivate;
+
+struct _ECalConfigMeetingStore {
+	EExtension parent;
+	ECalConfigMeetingStorePrivate *priv;
+};
+
+struct _ECalConfigMeetingStoreClass {
+	EExtensionClass parent_class;
+};
+
+GType		e_cal_config_meeting_store_get_type
+						(void) G_GNUC_CONST;
+void		e_cal_config_meeting_store_type_register
+						(GTypeModule *type_module);
 
 G_END_DECLS
 
