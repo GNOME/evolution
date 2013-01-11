620089c784f6e3c3dc9ee605a5f8ce08de593aa2
Matthew Barnes <mbarnes@redhat.com>
2013-01-11 08:40:07 -0500

    Add single-include barriers to a few e-util headers.

--- e-util/e-alert-sink.h
+++ e-util/e-alert-sink.h
@@ -16,6 +16,10 @@
  *
  */
 
+#if !defined (__E_UTIL_H_INSIDE__) && !defined (LIBEUTIL_COMPILATION)
+#error "Only <e-util/e-util.h> should be included directly."
+#endif
+
 #ifndef E_ALERT_SINK_H
 #define E_ALERT_SINK_H
 
--- e-util/e-xml-utils.h
+++ e-util/e-xml-utils.h
@@ -20,6 +20,10 @@
  *
  */
 
+#if !defined (__E_UTIL_H_INSIDE__) && !defined (LIBEUTIL_COMPILATION)
+#error "Only <e-util/e-util.h> should be included directly."
+#endif
+
 #ifndef __E_XML_UTILS__
 #define __E_XML_UTILS__
 
