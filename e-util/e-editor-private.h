e2f9e780d7a9bf6672bffdd8edf8cfc6b553f2d2
Matthew Barnes <mbarnes@redhat.com>
2013-02-07 16:17:34 -0500

    EEditor: Pack an EActivityBar widget.

--- e-util/e-editor-private.h
+++ e-util/e-editor-private.h
@@ -61,6 +61,7 @@ struct _EEditorPrivate {
 	GtkWidget *main_toolbar;
 	GtkWidget *edit_toolbar;
 	GtkWidget *html_toolbar;
+	GtkWidget *activity_bar;
 	GtkWidget *alert_bar;
 	GtkWidget *edit_area;
 
