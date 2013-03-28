a7cd179c901a620ea2f4e11589cccc960d8373fc
Matthew Barnes <mbarnes@redhat.com>
2013-03-27 20:50:34 -0400

    ESelectNamesEditable cleanups.

--- calendar/gui/e-select-names-editable.h
+++ calendar/gui/e-select-names-editable.h
@@ -21,46 +21,61 @@
  *
  */
 
-#ifndef __E_SELECT_NAMES_EDITABLE_H__
-#define __E_SELECT_NAMES_EDITABLE_H__
+#ifndef E_SELECT_NAMES_EDITABLE_H
+#define E_SELECT_NAMES_EDITABLE_H
+
+#include <e-util/e-util.h>
+
+/* Standard GObject macros */
+#define E_TYPE_SELECT_NAMES_EDITABLE \
+	(e_select_names_editable_get_type ())
+#define E_SELECT_NAMES_EDITABLE(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST \
+	((obj), E_TYPE_SELECT_NAMES_EDITABLE, ESelectNamesEditable))
+#define E_SELECT_NAMES_EDITABLE_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_CAST \
+	((cls), E_TYPE_SELECT_NAMES_EDITABLE, ESelectNamesEditableClass))
+#define E_IS_SELECT_NAMES_EDITABLE(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE \
+	((obj), E_TYPE_SELECT_NAMES_EDITABLE))
+#define E_IS_SELECT_NAMES_EDITABLE_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_TYPE \
+	((cls), E_TYPE_SELECT_NAMES_EDITABLE))
+#define E_SELECT_NAMES_EDITABLE_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS \
+	((obj), E_TYPE_SELECT_NAMES_EDITABLE, ESelectNamesEditableClass))
 
 G_BEGIN_DECLS
 
-#define E_TYPE_SELECT_NAMES_EDITABLE	     (e_select_names_editable_get_type ())
-#define E_SELECT_NAMES_EDITABLE(o)	     (G_TYPE_CHECK_INSTANCE_CAST ((o), E_TYPE_SELECT_NAMES_EDITABLE, ESelectNamesEditable))
-#define E_SELECT_NAMES_EDITABLE_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST ((k), E_TYPE_SELECT_NAMES_EDITABLE, ESelectNamesEditableClass))
-#define E_IS_SELECT_NAMES_EDITABLE(o)	     (G_TYPE_CHECK_INSTANCE_TYPE ((o), E_TYPE_SELECT_NAMES_EDITABLE))
-#define E_IS_SELECT_NAMES_EDITABLE_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((o), E_TYPE_SELECT_NAMES_EDITABLE))
-#define E_SELECT_NAMES_EDITABLE_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), E_TYPE_SELECT_NAMES_EDITABLE, ESelectNamesEditableClass))
-
-typedef struct _ESelectNamesEditable      ESelectNamesEditable;
+typedef struct _ESelectNamesEditable ESelectNamesEditable;
 typedef struct _ESelectNamesEditableClass ESelectNamesEditableClass;
-typedef struct _ESelectNamesEditablePriv  ESelectNamesEditablePriv;
+typedef struct _ESelectNamesEditablePrivate ESelectNamesEditablePrivate;
 
-struct _ESelectNamesEditable
-{
+struct _ESelectNamesEditable {
 	ENameSelectorEntry parent;
-
-	ESelectNamesEditablePriv *priv;
+	ESelectNamesEditablePrivate *priv;
 };
 
-struct _ESelectNamesEditableClass
-{
+struct _ESelectNamesEditableClass {
 	ENameSelectorEntryClass parent_class;
 };
 
-GType      e_select_names_editable_get_type (void);
-
-ESelectNamesEditable *e_select_names_editable_new (void);
-
-gchar *e_select_names_editable_get_email (ESelectNamesEditable *esne);
-GList *e_select_names_editable_get_emails (ESelectNamesEditable *esne);
-
-gchar *e_select_names_editable_get_name (ESelectNamesEditable *esne);
-GList *e_select_names_editable_get_names (ESelectNamesEditable *esne);
-
-void   e_select_names_editable_set_address (ESelectNamesEditable *esne, const gchar *name, const gchar *email);
+GType		e_select_names_editable_get_type
+						(void) G_GNUC_CONST;
+GtkWidget *	e_select_names_editable_new	(void);
+gchar *		e_select_names_editable_get_email
+						(ESelectNamesEditable *esne);
+GList *		e_select_names_editable_get_emails
+						(ESelectNamesEditable *esne);
+gchar *		e_select_names_editable_get_name
+						(ESelectNamesEditable *esne);
+GList *		e_select_names_editable_get_names
+						(ESelectNamesEditable *esne);
+void		e_select_names_editable_set_address
+						(ESelectNamesEditable *esne,
+						 const gchar *name,
+						 const gchar *email);
 
 G_END_DECLS
 
-#endif /* __E_SELECT_NAMES_EDITABLE_H__ */
+#endif /* E_SELECT_NAMES_EDITABLE_H */
