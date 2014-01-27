97fc8e626506781240acce37d253b529430fe87d
Milan Crha <mcrha@redhat.com>
2014-01-27 19:16:58 +0100

    Bug #721213 - Character encoding combo opened empty

--- e-util/e-action-combo-box.c
+++ e-util/e-action-combo-box.c
@@ -363,6 +363,9 @@ action_combo_box_constructed (GObject *object)
 
 	combo_box = GTK_COMBO_BOX (object);
 
+	/* Chain up to parent's constructed() method. */
+	G_OBJECT_CLASS (e_action_combo_box_parent_class)->constructed (object);
+
 	/* This needs to happen after constructor properties are set
 	 * so that GtkCellLayout.get_area() returns something valid. */
 
