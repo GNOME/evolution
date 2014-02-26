8357120ccb1c74126e11b13f43931db42ac5cb35
Tomas Popela <tpopela@redhat.com>
2014-02-26 11:06:33 +0100

    EEditor: Avoid crash when no proxies are returned

--- e-util/e-editor.c
+++ e-util/e-editor.c
@@ -289,8 +289,10 @@ editor_spell_checkers_foreach (EEditor *editor,
 		 *     child of the proxy widget. */
 		gtk_ui_manager_ensure_update (manager);
 		proxies = gtk_action_get_proxies (action);
-		child = gtk_bin_get_child (proxies->data);
-		g_object_set (child, "use-markup", TRUE, NULL);
+		if (proxies->data) {
+			child = gtk_bin_get_child (proxies->data);
+			g_object_set (child, "use-markup", TRUE, NULL);
+		}
 
 		g_free (action_name);
 		g_free (action_label);
