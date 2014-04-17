GNOME/evolution

commit b0106cc84284bad5d8d84192ff8aab54bb06ae1d

EMFolderTreeModel: Fix subscribing to a folder with children.
Unsubscribe from a folder with children, then re-subscribe.
The children will no longer be reachable in an EMFolderTree.

This is because we were calling em_folder_tree_model_set_folder_info()
claiming we have complete folder subtree info, when in fact we don't.

mail/em-folder-tree-model.c
@@ -1408,8 +1408,10 @@ folder_tree_model_folder_subscribed_cb (CamelStore *store,
 
 	gtk_tree_store_append (GTK_TREE_STORE (model), &iter, &parent);
 
+	/* The CamelFolderInfo does not contain child information,
+	 * so pass fully_loaded=FALSE to indicate it's incomplete. */
 	em_folder_tree_model_set_folder_info (
-		EM_FOLDER_TREE_MODEL (model), &iter, store, fi, TRUE);
+		EM_FOLDER_TREE_MODEL (model), &iter, store, fi, FALSE);
 }
 
 static void

commit 61695f06c1644138ec13c9200daf1044b655e34a

Newly created folders always marked as having subfolders
This corrects, a bit, a regression caused by commit b0106cc8, reported at
https://bugzilla.gnome.org/show_bug.cgi?id=722698#c13

mail/em-folder-tree-model.c
@@ -1429,10 +1429,9 @@ folder_tree_model_folder_subscribed_cb (CamelStore *store,
 
 	gtk_tree_store_append (GTK_TREE_STORE (model), &iter, &parent);
 
-	/* The CamelFolderInfo does not contain child information,
-	 * so pass fully_loaded=FALSE to indicate it's incomplete. */
 	em_folder_tree_model_set_folder_info (
-		EM_FOLDER_TREE_MODEL (model), &iter, store, fi, FALSE);
+		EM_FOLDER_TREE_MODEL (model), &iter, store, fi,
+		(fi->flags & (CAMEL_FOLDER_NOINFERIORS | CAMEL_FOLDER_NOCHILDREN)) != 0);
 }
 
 static void