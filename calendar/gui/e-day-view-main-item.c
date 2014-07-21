fc55dedaf1c3d00d6b4e117d988f2bafe2ca791f
Milan Crha <mcrha@redhat.com>
2014-07-21 15:45:48 +0200

    Always use selected color for selected region in EDayView
    It used focused or unfocused selected color, but the EDayView itself
    is not focused at all, it's main_item->parent.canvas instead. Instead
    of trying to hunt for this (and updates when the focused widget changes)
    just use the same selected color unconditionally, just the way the other
    views do it.

--- calendar/gui/e-day-view-main-item.c
+++ calendar/gui/e-day-view-main-item.c
@@ -1147,7 +1147,7 @@ day_view_main_item_draw (GnomeCanvasItem *canvas_item,
 
 			if (can_draw_in_region (draw_region, rect_x, rect_y, rect_width, rect_height)) {
 				cairo_save (cr);
-				gdk_cairo_set_source_color (cr, &day_view->colors[gtk_widget_has_focus (GTK_WIDGET (day_view)) ? E_DAY_VIEW_COLOR_BG_SELECTED : E_DAY_VIEW_COLOR_BG_SELECTED_UNFOCUSSED]);
+				gdk_cairo_set_source_color (cr, &day_view->colors[E_DAY_VIEW_COLOR_BG_SELECTED]);
 				cairo_rectangle (cr, rect_x, rect_y, rect_width, rect_height);
 				cairo_fill (cr);
 				cairo_restore (cr);
