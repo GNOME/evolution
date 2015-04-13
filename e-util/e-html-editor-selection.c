GNOME/evolution

commit 3f19190f18a616c076d99973dfefd2e3d1b0f5d8

Fix regressions after "Fix various leaks of WebKit DOM objects"

e-util/e-html-editor-selection.c
@@ -111,7 +111,6 @@ static WebKitDOMRange *
 html_editor_selection_get_current_range (EHTMLEditorSelection *selection)
 {
 	EHTMLEditorView *view;
-	WebKitWebView *web_view;
 	WebKitDOMDocument *document;
 	WebKitDOMDOMWindow *dom_window;
 	WebKitDOMDOMSelection *dom_selection;
@@ -120,26 +119,26 @@ html_editor_selection_get_current_range (EHTMLEditorSelection *selection)
 	view = e_html_editor_selection_ref_html_editor_view (selection);
 	g_return_val_if_fail (view != NULL, NULL);
 
-	web_view = WEBKIT_WEB_VIEW (view);
-
-	document = webkit_web_view_get_dom_document (web_view);
+	document = webkit_web_view_get_dom_document (WEBKIT_WEB_VIEW (view));
 	dom_window = webkit_dom_document_get_default_view (document);
 	if (!dom_window)
-		goto exit;
+		return NULL;
 
 	dom_selection = webkit_dom_dom_window_get_selection (dom_window);
-	if (!WEBKIT_DOM_IS_DOM_SELECTION (dom_selection))
-		goto exit;
+	if (!dom_selection) {
+		g_object_unref (dom_window);
+		return NULL;
+	}
 
-	if (webkit_dom_dom_selection_get_range_count (dom_selection) < 1)
+	if (webkit_dom_dom_selection_get_range_count (dom_selection) < 1) {
 		goto exit;
+	}
 
 	range = webkit_dom_dom_selection_get_range_at (dom_selection, 0, NULL);
 
  exit:
 	g_object_unref (dom_selection);
 	g_object_unref (dom_window);
-	g_object_unref (view);
 
 	return range;
 }

e-util/e-html-editor-view.c
@@ -489,7 +489,7 @@ perform_spell_check (WebKitDOMDOMSelection *dom_selection,
 		actual = webkit_dom_dom_selection_get_range_at (
 			dom_selection, 0, NULL);
 	}
-	g_object_unref (actual);
+	g_clear_object (&actual);
 }
 
 void

commit 3a123fee253fc39b50b34bc727d56ccf3af39065

Bug 747575 - EHTMLEditorView never freed
Main cause was regression after commit 3f19190f18a616c076d99973dfefd2e3d1b0f5d8
and also fixed another leak of EHTMLEditorView.

e-util/e-html-editor-selection.c
@@ -113,6 +113,7 @@ html_editor_selection_get_current_range (EHTMLEditorSelection *selection)
 	g_return_val_if_fail (view != NULL, NULL);
 
 	document = webkit_web_view_get_dom_document (WEBKIT_WEB_VIEW (view));
+	g_object_unref (view);
 	dom_window = webkit_dom_document_get_default_view (document);
 	if (!dom_window)
 		return NULL;
@@ -123,12 +124,10 @@ html_editor_selection_get_current_range (EHTMLEditorSelection *selection)
 		return NULL;
 	}
 
-	if (webkit_dom_dom_selection_get_range_count (dom_selection) < 1) {
+	if (webkit_dom_dom_selection_get_range_count (dom_selection) < 1)
 		goto exit;
-	}
 
 	range = webkit_dom_dom_selection_get_range_at (dom_selection, 0, NULL);
-
  exit:
 	g_object_unref (dom_selection);
 	g_object_unref (dom_window);
@@ -3164,8 +3163,10 @@ e_html_editor_selection_set_font_size (EHTMLEditorSelection *selection,
 	g_return_if_fail (view != NULL);
 
 	current_font_size = e_html_editor_selection_get_font_size (selection);
-	if (current_font_size == font_size)
+	if (current_font_size == font_size) {
+		g_object_unref (view);
 		return;
+	}
 
 	e_html_editor_selection_save (selection);
 