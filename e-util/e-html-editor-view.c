48dd0aedc35ffc7e9474c74fdba4a7155d6da125
Tomas Popela <tpopela@redhat.com>
2015-04-30 08:53:36 +0200

    Bug 748679 - Cannot middle-click paste text from composer
    When pasting with middle click move the caret to the right position even
    if the selection is not collapsed.

--- e-util/e-html-editor-view.c
+++ e-util/e-html-editor-view.c
@@ -3588,8 +3588,7 @@ html_editor_view_button_press_event (GtkWidget *widget,
 
 	if (event->button == 2) {
 		/* Middle click paste */
-		if (collapsed)
-			html_editor_view_move_selection_on_point (widget);
+		html_editor_view_move_selection_on_point (widget);
 		g_signal_emit (widget, signals[PASTE_PRIMARY_CLIPBOARD], 0);
 		event_handled = TRUE;
 	} else if (event->button == 3) {
