62457130f0456846c2a251dbcead8effda6f2431
Tomas Popela <tpopela@redhat.com>
2016-01-07 14:17:27 +0100

    EHTMLEditorView - Cannot append text to links if non-breaking space is presented

--- e-util/e-html-editor-view.c
+++ e-util/e-html-editor-view.c
@@ -2857,7 +2857,7 @@ body_input_event_cb (WebKitDOMElement *element,
 
 			text = webkit_dom_node_get_text_content (node);
 
-			if (text && *text && !strstr (text, " ")) {
+			if (text && *text && *text != ' ' && !g_str_has_prefix (text, UNICODE_NBSP)) {
 				gboolean valid = FALSE;
 
 				if (*text == '?' && strlen (text) > 1)
