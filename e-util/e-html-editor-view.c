GNOME/evolution

commit 6c09c26dcbfcf7eab6fbec6699078ab9d4ae1de5

EHTMLEditorView - Pressing the Return key in the beginning of list item will delete the item
First check if there is something after the selection end, if nothing move to the
selection start, not in reversed order.

e-util/e-html-editor-view.c
@@ -5397,6 +5397,19 @@ selection_is_in_empty_list_item (WebKitDOMNode *selection_start_marker)
 
 	sibling = webkit_dom_node_get_previous_sibling (WEBKIT_DOM_NODE (selection_start_marker));
 
+	/* Selection needs to be collapsed. */
+	sibling = webkit_dom_node_get_next_sibling (WEBKIT_DOM_NODE (selection_start_marker));
+	if (!e_html_editor_node_is_selection_position_node (sibling))
+		return FALSE;
+
+	/* After the selection end there could be just the BR element. */
+	sibling = webkit_dom_node_get_next_sibling (sibling);
+	if (sibling && !WEBKIT_DOM_IS_HTMLBR_ELEMENT (sibling))
+	       return FALSE;
+
+	if (sibling && webkit_dom_node_get_next_sibling (sibling))
+		return FALSE;
+
 	if (!sibling)
 		return TRUE;
 
@@ -5417,19 +5430,6 @@ selection_is_in_empty_list_item (WebKitDOMNode *selection_start_marker)
 	}
 
 	g_free (text);
-
-	/* Selection needs to be collapsed. */
-	sibling = webkit_dom_node_get_next_sibling (WEBKIT_DOM_NODE (selection_start_marker));
-	if (!e_html_editor_node_is_selection_position_node (sibling))
-		return FALSE;
-
-	/* After the selection end there could be just the BR element. */
-	sibling = webkit_dom_node_get_next_sibling (sibling);
-	if (sibling && !WEBKIT_DOM_IS_HTMLBR_ELEMENT (sibling))
-	       return FALSE;
-
-	if (sibling && webkit_dom_node_get_next_sibling (sibling))
-		return FALSE;
 
 	return TRUE;
 }

commit 76af63a2c1ed1185accd9aa6beecfa5e9dfe2c64

Bug 762429 - List item is removed when the Return is pressed on its end
Regression after commit 6c09c26.

e-util/e-html-editor-view.c
@@ -5435,8 +5435,6 @@ selection_is_in_empty_list_item (WebKitDOMNode *selection_start_marker)
 	gchar *text;
 	WebKitDOMNode *sibling;
 
-	sibling = webkit_dom_node_get_previous_sibling (WEBKIT_DOM_NODE (selection_start_marker));
-
 	/* Selection needs to be collapsed. */
 	sibling = webkit_dom_node_get_next_sibling (WEBKIT_DOM_NODE (selection_start_marker));
 	if (!e_html_editor_node_is_selection_position_node (sibling))
@@ -5450,8 +5448,7 @@ selection_is_in_empty_list_item (WebKitDOMNode *selection_start_marker)
 	if (sibling && webkit_dom_node_get_next_sibling (sibling))
 		return FALSE;
 
-	if (!sibling)
-		return TRUE;
+	sibling = webkit_dom_node_get_previous_sibling (WEBKIT_DOM_NODE (selection_start_marker));
 
 	/* Only text node with the zero width space character is allowed. */
 	if (!WEBKIT_DOM_IS_TEXT (sibling))