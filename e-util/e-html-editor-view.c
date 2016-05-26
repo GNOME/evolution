76c267a8051fe1d27e9b78788ab0c964382472f4
Tomas Popela <tpopela@redhat.com>
2016-05-26 15:34:31 +0200

    Bug 766796 - No space after double hyphen in autogenerated signature
    The space is always there, but as it was not in the PRE element WebKit hide it.

--- e-util/e-html-editor-view.c
+++ e-util/e-html-editor-view.c
@@ -15757,9 +15757,17 @@ e_html_editor_view_insert_signature (EHTMLEditorView *view,
 			;  /* skip */
 		else if (e_util_strstrcase (signature_text, delim_nl) != NULL)
 			;  /* skip */
-		else
+		else {
+			WebKitDOMElement *pre_delimiter;
+
+			pre_delimiter = webkit_dom_document_create_element (document, "pre", NULL);
 			webkit_dom_html_element_set_inner_html (
-				WEBKIT_DOM_HTML_ELEMENT (insert_signature_in), delim, NULL);
+				WEBKIT_DOM_HTML_ELEMENT (pre_delimiter), delim, NULL);
+			webkit_dom_node_append_child (
+				WEBKIT_DOM_NODE (insert_signature_in),
+				WEBKIT_DOM_NODE (pre_delimiter),
+				NULL);
+		}
 	}
 
 	if (converted_signature) {
