50c17c762961ce513f5ba40e985b32d3f117f5ac
Tomas Popela <tpopela@redhat.com>
2016-06-09 17:47:40 +0200

    Rename web-inspector to webkit-inspector

--- modules/webkit-inspector/evolution-webkit-inspector.c
+++ modules/webkit-inspector/evolution-webkit-inspector.c
@@ -1,5 +1,5 @@
 /*
- * evolution-web-inspector.c
+ * evolution-webkit-inspector.c
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -24,28 +24,28 @@
 #include <e-util/e-util.h>
 
 /* Standard GObject macros */
-#define E_TYPE_WEB_INSPECTOR \
-	(e_web_inspector_get_type ())
-#define E_WEB_INSPECTOR(obj) \
+#define E_TYPE_WEBKIT_INSPECTOR \
+	(e_webkit_inspector_get_type ())
+#define E_WEBKIT_INSPECTOR(obj) \
 	(G_TYPE_CHECK_INSTANCE_CAST \
-	((obj), E_TYPE_WEB_INSPECTOR, EWebInspector))
+	((obj), E_TYPE_WEBKIT_INSPECTOR, EWebKitInspector))
 
 /* <Control>+<Shift>+I */
-#define WEB_INSPECTOR_MOD  (GDK_CONTROL_MASK | GDK_SHIFT_MASK)
-#define WEB_INSPECTOR_KEY  (GDK_KEY_I)
+#define WEBKIT_INSPECTOR_MOD  (GDK_CONTROL_MASK | GDK_SHIFT_MASK)
+#define WEBKIT_INSPECTOR_KEY  (GDK_KEY_I)
 
-#define WEB_INSPECTOR_SHORTCUT_SHOW(event) \
-	((((event)->state & WEB_INSPECTOR_MOD) == WEB_INSPECTOR_MOD) && \
-	((event)->keyval == WEB_INSPECTOR_KEY))
+#define WEBKIT_INSPECTOR_SHORTCUT_SHOW(event) \
+	((((event)->state & WEBKIT_INSPECTOR_MOD) == WEBKIT_INSPECTOR_MOD) && \
+	((event)->keyval == WEBKIT_INSPECTOR_KEY))
 
-typedef struct _EWebInspector EWebInspector;
-typedef struct _EWebInspectorClass EWebInspectorClass;
+typedef struct _EWebKitInspector EWebKitInspector;
+typedef struct _EWebKitInspectorClass EWebKitInspectorClass;
 
-struct _EWebInspector {
+struct _EWebKitInspector {
 	EExtension parent;
 };
 
-struct _EWebInspectorClass {
+struct _EWebKitInspectorClass {
 	EExtensionClass parent_class;
 };
 
@@ -54,12 +54,12 @@ void e_module_load (GTypeModule *type_module);
 void e_module_unload (GTypeModule *type_module);
 
 /* Forward Declarations */
-GType e_web_inspector_get_type (void);
+GType e_webkit_inspector_get_type (void);
 
-G_DEFINE_DYNAMIC_TYPE (EWebInspector, e_web_inspector, E_TYPE_EXTENSION)
+G_DEFINE_DYNAMIC_TYPE (EWebKitInspector, e_webkit_inspector, E_TYPE_EXTENSION)
 
 static WebKitWebView *
-web_inspector_get_web_view (EWebInspector *extension)
+webkit_inspector_get_web_view (EWebKitInspector *extension)
 {
 	EExtensible *extensible;
 
@@ -69,7 +69,7 @@ web_inspector_get_web_view (EWebInspector *extension)
 }
 
 static gboolean
-web_inspector_key_press_event_cb (WebKitWebView *web_view,
+webkit_inspector_key_press_event_cb (WebKitWebView *web_view,
                                   GdkEventKey *event)
 {
 	WebKitWebInspector *inspector;
@@ -77,7 +77,7 @@ web_inspector_key_press_event_cb (WebKitWebView *web_view,
 
 	inspector = webkit_web_view_get_inspector (web_view);
 
-	if (WEB_INSPECTOR_SHORTCUT_SHOW (event)) {
+	if (WEBKIT_INSPECTOR_SHORTCUT_SHOW (event)) {
 		webkit_web_inspector_show (inspector);
 		handled = TRUE;
 	}
@@ -86,52 +86,52 @@ web_inspector_key_press_event_cb (WebKitWebView *web_view,
 }
 
 static void
-web_inspector_constructed (GObject *object)
+webkit_inspector_constructed (GObject *object)
 {
-	EWebInspector *extension;
+	EWebKitInspector *extension;
 	WebKitWebView *web_view;
 	WebKitSettings *settings;
 
 	/* Chain up to parent's method. */
-	G_OBJECT_CLASS (e_web_inspector_parent_class)->constructed (object);
+	G_OBJECT_CLASS (e_webkit_inspector_parent_class)->constructed (object);
 
-	extension = E_WEB_INSPECTOR (object);
-	web_view = web_inspector_get_web_view (extension);
+	extension = E_WEBKIT_INSPECTOR (object);
+	web_view = webkit_inspector_get_web_view (extension);
 	settings = webkit_web_view_get_settings (web_view);
 	webkit_settings_set_enable_developer_extras (settings, TRUE);
 
 	g_signal_connect (
 		web_view, "key-press-event",
-		G_CALLBACK (web_inspector_key_press_event_cb), NULL);
+		G_CALLBACK (webkit_inspector_key_press_event_cb), NULL);
 }
 
 static void
-e_web_inspector_class_init (EWebInspectorClass *class)
+e_webkit_inspector_class_init (EWebKitInspectorClass *class)
 {
 	GObjectClass *object_class;
 	EExtensionClass *extension_class;
 
 	object_class = G_OBJECT_CLASS (class);
-	object_class->constructed = web_inspector_constructed;
+	object_class->constructed = webkit_inspector_constructed;
 
 	extension_class = E_EXTENSION_CLASS (class);
 	extension_class->extensible_type = WEBKIT_TYPE_WEB_VIEW;
 }
 
 static void
-e_web_inspector_class_finalize (EWebInspectorClass *class)
+e_webkit_inspector_class_finalize (EWebKitInspectorClass *class)
 {
 }
 
 static void
-e_web_inspector_init (EWebInspector *extension)
+e_webkit_inspector_init (EWebKitInspector *extension)
 {
 }
 
 G_MODULE_EXPORT void
 e_module_load (GTypeModule *type_module)
 {
-	e_web_inspector_register_type (type_module);
+	e_webkit_inspector_register_type (type_module);
 }
 
 G_MODULE_EXPORT void
