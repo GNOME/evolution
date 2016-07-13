8a033737f97316011ffae2fbb419f81b34445e3d
Tomas Popela <tpopela@redhat.com>
2016-07-13 12:15:51 +0200

    EWebView - Clicking on image wrapped in link won't open the link in the external browser

--- e-util/e-web-view.c
+++ e-util/e-web-view.c
@@ -600,7 +600,8 @@ web_view_decide_policy_cb (EWebView *web_view,
 	WebKitURIRequest *request;
 	const gchar *uri, *view_uri;
 
-	if (type != WEBKIT_POLICY_DECISION_TYPE_NAVIGATION_ACTION)
+	if (type != WEBKIT_POLICY_DECISION_TYPE_NAVIGATION_ACTION &&
+	    type != WEBKIT_POLICY_DECISION_TYPE_NEW_WINDOW_ACTION)
 		return FALSE;
 
 	navigation_decision = WEBKIT_NAVIGATION_POLICY_DECISION (decision);
