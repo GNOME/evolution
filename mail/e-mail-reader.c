8ecf37301969f91abe33a2ac74de60e46e9dcd66
Milan Crha <mcrha@redhat.com>
2016-08-03 13:21:12 +0200

    Bug 768683 - Cannot mark as spam non-spam message in a real Junk folder

--- mail/e-mail-reader.c
+++ mail/e-mail-reader.c
@@ -3869,9 +3869,7 @@ mail_reader_update_actions (EMailReader *reader,
 	gtk_action_set_sensitive (action, sensitive);
 
 	action_name = "mail-mark-junk";
-	sensitive =
-		selection_has_not_junk_messages &&
-		!(state & E_MAIL_READER_FOLDER_IS_JUNK);
+	sensitive = selection_has_not_junk_messages;
 	action = e_mail_reader_get_action (reader, action_name);
 	gtk_action_set_sensitive (action, sensitive);
 
@@ -4526,7 +4524,6 @@ e_mail_reader_check_state (EMailReader *reader)
 	gboolean has_mail_note = FALSE;
 	gboolean have_enabled_account = FALSE;
 	gboolean drafts_or_outbox = FALSE;
-	gboolean store_supports_vjunk = FALSE;
 	gboolean is_mailing_list;
 	gboolean is_junk_folder = FALSE;
 	gboolean is_vtrash_folder = FALSE;
@@ -4550,7 +4547,6 @@ e_mail_reader_check_state (EMailReader *reader)
 		gchar *archive_folder;
 
 		store = camel_folder_get_parent_store (folder);
-		store_supports_vjunk = (store->flags & CAMEL_STORE_VJUNK);
 		is_junk_folder =
 			(folder->folder_flags & CAMEL_FOLDER_IS_JUNK) != 0;
 		is_vtrash_folder = (store->flags & CAMEL_STORE_VTRASH) != 0 && (folder->folder_flags & CAMEL_FOLDER_IS_TRASH) != 0;
@@ -4595,7 +4591,7 @@ e_mail_reader_check_state (EMailReader *reader)
 		if (drafts_or_outbox) {
 			has_junk = FALSE;
 			has_not_junk = FALSE;
-		} else if (store_supports_vjunk) {
+		} else {
 			guint32 bitmask;
 
 			/* XXX Strictly speaking, this logic is correct.
@@ -4618,10 +4614,6 @@ e_mail_reader_check_state (EMailReader *reader)
 				has_junk = TRUE;
 				has_not_junk = TRUE;
 			}
-
-		} else {
-			has_junk = TRUE;
-			has_not_junk = TRUE;
 		}
 
 		if (flags & CAMEL_MESSAGE_DELETED)
