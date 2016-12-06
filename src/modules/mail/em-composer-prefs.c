001fa1b3e0e25d61d1b68e4f521fb54db8ead34f
Milan Crha <mcrha@redhat.com>
2016-12-06 17:21:38 +0100

    Add a Confirmation option 'prompt-on-composer-mode-switch' into Preferences

--- src/modules/mail/em-composer-prefs.c
+++ src/modules/mail/em-composer-prefs.c
@@ -1154,6 +1154,12 @@ em_composer_prefs_construct (EMComposerPrefs *prefs,
 		widget, "active",
 		G_SETTINGS_BIND_DEFAULT);
 
+	widget = e_builder_get_widget (prefs->builder, "chkPromptComposerModeSwitch");
+	g_settings_bind (
+		settings, "prompt-on-composer-mode-switch",
+		widget, "active",
+		G_SETTINGS_BIND_DEFAULT);
+
 	widget = e_builder_get_widget (prefs->builder, "chkAutoSmileys");
 	g_settings_bind (
 		settings, "composer-magic-smileys",
