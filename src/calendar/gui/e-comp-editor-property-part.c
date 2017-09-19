18c089f1cf3a28ab5c66441ba3b1ed84a13f1e33
Milan Crha <mcrha@redhat.com>
2017-09-19 12:21:25 +0200

    Correct runtime warning when creating All Day Appointment
    When creating an All Day Appointment, like from the File->New-> menu,
    Evolution could show a runtime warning like the one below. This
    corrects the code to avoid the runtime warning.
    
    evolution-util-CRITICAL **: e_date_edit_set_time_of_day: assertion 'allow_no_date_set' failed

--- src/calendar/gui/e-comp-editor-property-part.c
+++ src/calendar/gui/e-comp-editor-property-part.c
@@ -834,7 +834,7 @@ e_comp_editor_property_part_datetime_set_value (ECompEditorPropertyPartDatetime 
 			e_date_edit_set_time_of_day (date_edit, value.hour, value.minute);
 		else if (e_date_edit_get_show_time (date_edit))
 			e_date_edit_set_time_of_day (date_edit, 0, 0);
-		else
+		else if (e_date_edit_get_allow_no_date_set (date_edit))
 			e_date_edit_set_time_of_day (date_edit, -1, -1);
 
 		e_comp_editor_property_part_datetime_set_date_only (part_datetime, value.is_date);
