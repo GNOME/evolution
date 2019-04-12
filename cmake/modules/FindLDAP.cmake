4569f310428c32948679301046026856b5da2958
Дилян Палаузов <dilyan.palauzov@aegee.org>
2019-04-12 12:37:39 +0200

    eds-I#99 - Fails to build against git master of OpenLDAP (vendor version check)
    Related to https://gitlab.gnome.org/GNOME/evolution-data-server/issues/99

--- cmake/modules/FindLDAP.cmake
+++ cmake/modules/FindLDAP.cmake
@@ -86,7 +86,8 @@ set(CMAKE_REQUIRED_LIBRARIES "${LDAP_LIBS}")
 if(WITH_OPENLDAP)
 	CHECK_C_SOURCE_COMPILES("#include \"ldap.h\"
 				int main(void) {
-					#if !defined(LDAP_VENDOR_VERSION) || LDAP_VENDOR_VERSION < 20000
+					/* LDAP_VENDOR_VERSION is 0 if OpenLDAP is built from git/master */
+					#if !defined(LDAP_VENDOR_VERSION) || (LDAP_VENDOR_VERSION != 0 && LDAP_VENDOR_VERSION < 20000)
 					#error OpenLDAP version not at least 2.0
 					#endif
 					return 0; }" openldap_2_x)
