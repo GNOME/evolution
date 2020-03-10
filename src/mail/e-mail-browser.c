GNOME/evolution

commit eba73e989f7a561645f1896c23dc2dfaadf015c5

EMailBrowser: Escape key press closes window, instead of the search bar
When pressing Escape key in the search bar, the search bar should close,
instead of the whole window.

src/mail/e-mail-browser.c
@@ -486,7 +486,15 @@ mail_browser_key_press_event_cb (GtkWindow *mail_browser,
 		return TRUE;
 	}
 
-	return e_mail_display_need_key_event (mail_display, event);
+	if (e_mail_display_need_key_event (mail_display, event))
+		return TRUE;
+
+	if (event->keyval == GDK_KEY_Escape) {
+		e_mail_browser_close (E_MAIL_BROWSER (mail_browser));
+		return TRUE;
+	}
+
+	return FALSE;
 }
 
 static void
@@ -935,20 +943,6 @@ mail_browser_constructed (GObject *object)
 	e_extensible_load_extensions (E_EXTENSIBLE (object));
 }
 
-static gboolean
-mail_browser_key_press_event (GtkWidget *widget,
-                              GdkEventKey *event)
-{
-	if (event->keyval == GDK_KEY_Escape) {
-		e_mail_browser_close (E_MAIL_BROWSER (widget));
-		return TRUE;
-	}
-
-	/* Chain up to parent's key_press_event() method. */
-	return GTK_WIDGET_CLASS (e_mail_browser_parent_class)->
-		key_press_event (widget, event);
-}
-
 static GtkActionGroup *
 mail_browser_get_action_group (EMailReader *reader,
                                EMailReaderActionGroup group)
@@ -1138,7 +1132,6 @@ static void
 e_mail_browser_class_init (EMailBrowserClass *class)
 {
 	GObjectClass *object_class;
-	GtkWidgetClass *widget_class;
 
 	g_type_class_add_private (class, sizeof (EMailBrowserPrivate));
 
@@ -1147,9 +1140,6 @@ e_mail_browser_class_init (EMailBrowserClass *class)
 	object_class->get_property = mail_browser_get_property;
 	object_class->dispose = mail_browser_dispose;
 	object_class->constructed = mail_browser_constructed;
-
-	widget_class = GTK_WIDGET_CLASS (class);
-	widget_class->key_press_event = mail_browser_key_press_event;
 
 	g_object_class_install_property (
 		object_class,

commit 0007c100fa40d1f34427d76f907d12a630bab7a1

EMailBrowser: Cannot be closed with Escape key press
This fixes a regression from commit eba73e989f7a561645f1896c23dc2dfaadf015c5.

src/mail/e-mail-browser.c
@@ -467,7 +467,6 @@ mail_browser_key_press_event_cb (GtkWindow *mail_browser,
 	if (!event || (event->state & (GDK_CONTROL_MASK | GDK_SHIFT_MASK | GDK_MOD1_MASK)) != 0 ||
 	    event->keyval == GDK_KEY_Tab ||
 	    event->keyval == GDK_KEY_Return ||
-	    event->keyval == GDK_KEY_Escape ||
 	    event->keyval == GDK_KEY_KP_Tab ||
 	    event->keyval == GDK_KEY_KP_Enter)
 		return event && e_mail_display_need_key_event (mail_display, event);
@@ -477,7 +476,7 @@ mail_browser_key_press_event_cb (GtkWindow *mail_browser,
 	if (focused && (GTK_IS_ENTRY (focused) || GTK_IS_EDITABLE (focused) ||
 	    (GTK_IS_TREE_VIEW (focused) && gtk_tree_view_get_search_column (GTK_TREE_VIEW (focused)) >= 0))) {
 		gtk_widget_event (focused, (GdkEvent *) event);
-		return TRUE;
+		return event->keyval != GDK_KEY_Escape;
 	}
 
 	if (e_web_view_get_need_input (E_WEB_VIEW (mail_display)) &&