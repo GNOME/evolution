f767d5ac1433ad76a4953da0c27cc7228c2beeec
Milan Crha <mcrha@redhat.com>
2020-09-01 17:25:00 +0200

    Remove unused function prototype declaration

--- src/modules/mail/e-mail-shell-view-private.h
+++ src/modules/mail/e-mail-shell-view-private.h
@@ -164,8 +164,6 @@ void		e_mail_shell_view_update_sidebar
 					(EMailShellView *mail_shell_view);
 void		e_mail_shell_view_update_send_receive_menus
 					(EMailShellView *mail_shell_view);
-GDBusProxy *	e_mail_shell_view_get_web_extension_proxy
-					(EMailShellView *mail_shell_view);
 void		e_mail_shell_view_rename_folder
 					(EMailShellView *mail_shell_view);
 
