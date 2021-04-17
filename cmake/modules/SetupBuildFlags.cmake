ed89af2702cf3e0681cbd44aef3dd50abda418c5
Matt Turner <mattst88@gmail.com>
2021-04-16 21:03:33 -0400

    eds-M!68 - SetupBuildFlags.cmake: Correct testing of two compiler/linker build flags
    Closes https://gitlab.gnome.org/GNOME/evolution-data-server/-/merge_requests/68

--- cmake/modules/SetupBuildFlags.cmake
+++ cmake/modules/SetupBuildFlags.cmake
@@ -20,7 +20,7 @@ macro(setup_build_flags _maintainer_mode)
 		-Wundef
 		-Wwrite-strings
 		-Wno-cast-function-type
-		-no-undefined
+		-Wl,--no-undefined
 		-fno-strict-aliasing
 	)
 
@@ -34,7 +34,7 @@ macro(setup_build_flags _maintainer_mode)
 	else(${_maintainer_mode})
 		list(APPEND proposed_flags
 			-Wno-deprecated-declarations
-			-Wno-missing-include-dir)
+			-Wno-missing-include-dirs)
 	endif(${_maintainer_mode})
 
 	list(APPEND proposed_c_flags
