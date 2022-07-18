3ed3b2a87f6c7c9199912bda9c0c44efc23eff6f
Milan Crha <mcrha@redhat.com>
2022-07-18 11:34:39 +0200

    I#1961 - Missing divider lines and paddings under GNOME
    Closes https://gitlab.gnome.org/GNOME/evolution/-/issues/1961

--- src/shell/e-shell-window.c
+++ src/shell/e-shell-window.c
@@ -1163,10 +1163,17 @@ e_shell_window_init (EShellWindow *shell_window)
 	gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
 		GTK_STYLE_PROVIDER (css_provider),
 		GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	g_clear_object (&css_provider);
 
 	/* If running on GNOME, force symbolic icons */
-	if (e_util_is_running_gnome ())
+	if (e_util_is_running_gnome ()) {
+		css_provider = gtk_css_provider_new ();
 		gtk_css_provider_load_from_data (css_provider, css_icons, -1, NULL);
+		gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
+			GTK_STYLE_PROVIDER (css_provider),
+			GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+		g_clear_object (&css_provider);
+	}
 
 	g_signal_connect (shell_window, "delete-event",
 		G_CALLBACK (shell_window_delete_event_cb), NULL);
