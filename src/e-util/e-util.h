122f34a347809ccc74573d9034c67a847fb86ea6
Corentin Noël <tintou@noel.tf>
2025-04-15 17:25:38 +0200

    M!196 - Remove ESimpleAsyncResult completely
    Closes: https://gitlab.gnome.org/GNOME/evolution/-/merge_requests/196

--- src/e-util/e-util.h
+++ src/e-util/e-util.h
@@ -197,7 +197,6 @@
 #include <e-util/e-selection-model-simple.h>
 #include <e-util/e-selection-model.h>
 #include <e-util/e-selection.h>
-#include <e-util/e-simple-async-result.h>
 #include <e-util/e-sorter-array.h>
 #include <e-util/e-sorter.h>
 #include <e-util/e-source-combo-box.h>
