7d730a91239471d23d95da64b81b53d49d89244a
Milan Crha <mcrha@redhat.com>
2026-09-11 08:35:42 +0200

    Remove Glade catalog build
    It was not used nor updated for years, neither by the maintainers, thus drop it.

--- src/glade/gladeevolution.c
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: (C) 2016 Red Hat, Inc. (www.redhat.com)
- * SPDX-License-Identifier: LGPL-2.1-or-later
- */
-
-/* Really nothing to be here, the glade catalog requires the module
-   and this source file is here "to have something to compile" only.
- */
